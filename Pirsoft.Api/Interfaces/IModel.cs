657dec6bb7729115fe089e97586ca4b1a8ec40f7
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-01-04 17:23:08 +0100

    TEST-90 factory pattern interface POC

--- Pirsoft.Api/Interfaces/IModel.cs
+++ Pirsoft.Api/Interfaces/IModel.cs
@@ -1,6 +1,10 @@
+using Pirsoft.Api.Enums;
+using Pirsoft.Api.Models;
+
 namespace Pirsoft.Api.Interfaces;
 
 public interface IModel
 {
-    
+    public EmployeeConcrete CreateEmployeeModel(Guid id, string firstName, string lastName, string email, string password, AccountType accountType, string pesel, string bankAccountNumber,
+        int departmentId, int seniorityInMonths, DateTime employmentStartDate, bool isActive, bool passwordReset, DateTime dateOfBirth, double grossSalary, PositionType positionType);
 }
---
+++ Pirsoft.Api/Models/EmployeeCreator.cs
@@ -0,0 +1,14 @@
+﻿using Pirsoft.Api.Factory;
+using Pirsoft.Api.Interfaces;
+
+namespace Pirsoft.Api.Models
+{
+    public class EmployeeCreator : Creator
+    {
+        public override IModel CreateModel(Guid Id, string FirstName)
+        {
+            //validate here
+            return new EmployeeConcrete();
+        }
+    }   
+}
