6e07c968a449fabe424cf4bb5c21866eebabf320
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-01-18 19:41:48 +0100

    TEST-90 added pesel validation with unit tests

---
+++ Pirsoft.Api/Validators/EmployeeModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Pirsoft.Api.Validators;
+
+public class EmployeeModelValidator
+{
+    public bool IsPeselValid(string validatedExpression)
+    {
+        var regex = new Regex("^[0-9]+$");
+
+        return regex.IsMatch(validatedExpression) && validatedExpression.Length==11;
+    }
+    
+    
+}
