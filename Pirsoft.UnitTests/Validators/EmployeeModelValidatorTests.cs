29d29f47df805cd8bb00ec9456492971b6eb6656
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-01-18 19:41:48 +0100

    TEST-90 added pesel validation with unit tests

---
+++ Pirsoft.UnitTests/Validators/EmployeeModelValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Pirsoft.Api.Validators;
+
+namespace Pirsoft.UnitTests.Validators;
+
+public class EmployeeModelValidatorTests
+{
+    private EmployeeModelValidator _sut = null!;
+    
+    [SetUp]
+    public void SetUp() =>_sut = new EmployeeModelValidator();
+
+    [Test]
+    public void PeselIsValidWithElevenNumbers() => _sut.IsPeselValid("12345678901").Should().BeTrue();
+
+    [Test]
+    public void PeselIsInvalidWithoutElevenCharacters() => _sut.IsPeselValid("123456789").Should().BeFalse();
+
+    [Test]
+    public void PeselIsInvalidWithDifferentCharactersThanNumbers() =>
+        _sut.IsPeselValid("123abc456de").Should().BeFalse();
+}
