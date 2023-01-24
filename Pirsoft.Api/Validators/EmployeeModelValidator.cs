6995c3a535298bde5e60c1930811f9305fd976b6
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-01-24 14:06:55 +0100

    TEST-90 added validation for bank account number with unit tests

--- Pirsoft.Api/Validators/EmployeeModelValidator.cs
+++ Pirsoft.Api/Validators/EmployeeModelValidator.cs
@@ -4,12 +4,17 @@ namespace Pirsoft.Api.Validators;
 
 public class EmployeeModelValidator
 {
-    public bool IsPeselValid(string validatedExpression)
+    public static bool IsPeselValid(string validatedExpression)
     {
         var regex = new Regex("^[0-9]+$");
 
         return regex.IsMatch(validatedExpression) && validatedExpression.Length==11;
     }
-    
-    
+
+    public static bool IsBankAccountNumberValid(string bankAccountNumber)
+    {
+        var regex = new Regex("^[0-9]+$");
+
+        return regex.IsMatch(bankAccountNumber) && bankAccountNumber.Length==26;
+    }
 }
--- Pirsoft.UnitTests/Validators/EmployeeModelValidatorTests.cs
+++ Pirsoft.UnitTests/Validators/EmployeeModelValidatorTests.cs
@@ -12,12 +12,22 @@ public class EmployeeModelValidatorTests
     public void SetUp() =>_sut = new EmployeeModelValidator();
 
     [Test]
-    public void PeselIsValidWithElevenNumbers() => _sut.IsPeselValid("12345678901").Should().BeTrue();
+    public void PeselIsValidWithElevenNumbers() => EmployeeModelValidator.IsPeselValid("12345678901").Should().BeTrue();
 
     [Test]
-    public void PeselIsInvalidWithoutElevenCharacters() => _sut.IsPeselValid("123456789").Should().BeFalse();
+    public void PeselIsInvalidWithoutElevenCharacters() => EmployeeModelValidator.IsPeselValid("123456789").Should().BeFalse();
 
     [Test]
     public void PeselIsInvalidWithDifferentCharactersThanNumbers() =>
-        _sut.IsPeselValid("123abc456de").Should().BeFalse();
+        EmployeeModelValidator.IsPeselValid("123abc456de").Should().BeFalse();
+    
+    [Test]
+    public void BankAccountIsValidWithTwentySixNumbers() => EmployeeModelValidator.IsBankAccountNumberValid("12345678901123456789012345").Should().BeTrue();
+    
+    [Test]
+    public void BankAccountIsInvalidWithoutTwentySixCharacters() => EmployeeModelValidator.IsBankAccountNumberValid("123123123").Should().BeFalse();
+
+    [Test]
+    public void BankAccountIsInvalidWithDifferentCharactersThanNumbers() => EmployeeModelValidator
+        .IsBankAccountNumberValid("123A567B901D234C67G90X23T5").Should().BeFalse();
 }
