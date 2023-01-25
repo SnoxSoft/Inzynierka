8126bec0a1c85dbb584af79452e35e239159866a
20497 <s20497@pjwstk.edu.pl>
2023-01-25 21:12:40 +0100

    TEST-90 fixed tests & static methods for employee model validator class

--- Pirsoft.Api/Validators/EmployeeModelValidator.cs
+++ Pirsoft.Api/Validators/EmployeeModelValidator.cs
@@ -5,21 +5,21 @@ namespace Pirsoft.Api.Validators;
 
 public class EmployeeModelValidator
 {
-    public static bool IsPeselValid(string validatedExpression)
+    public bool IsPeselValid(string validatedExpression)
     {
         var regex = new Regex("^[0-9]+$");
 
         return regex.IsMatch(validatedExpression) && validatedExpression.Length==11;
     }
 
-    public static bool IsBankAccountNumberValid(string bankAccountNumber)
+    public bool IsBankAccountNumberValid(string bankAccountNumber)
     {
         var regex = new Regex("^[0-9]+$");
 
         return regex.IsMatch(bankAccountNumber) && bankAccountNumber.Length==26;
     }
     
-    public static bool IsEmailAddressValid(string emailAddress)
+    public bool IsEmailAddressValid(string emailAddress)
     {
         try
         {
--- Pirsoft.UnitTests/Validators/EmployeeModelValidatorTests.cs
+++ Pirsoft.UnitTests/Validators/EmployeeModelValidatorTests.cs
@@ -9,33 +9,33 @@ public class EmployeeModelValidatorTests
     private EmployeeModelValidator _sut = null!;
     
     [SetUp]
-    public void SetUp() =>_sut = new EmployeeModelValidator();
+    public void SetUp() => _sut = new EmployeeModelValidator();
 
     [Test]
-    public void PeselIsValidWithElevenNumbers() => EmployeeModelValidator.IsPeselValid("12345678901").Should().BeTrue();
+    public void PeselIsValidWithElevenNumbers() => _sut.IsPeselValid("12345678901").Should().BeTrue();
 
     [Test]
-    public void PeselIsInvalidWithoutElevenCharacters() => EmployeeModelValidator.IsPeselValid("123456789").Should().BeFalse();
+    public void PeselIsInvalidWithoutElevenCharacters() => _sut.IsPeselValid("123456789").Should().BeFalse();
 
     [Test]
     public void PeselIsInvalidWithDifferentCharactersThanNumbers() =>
-        EmployeeModelValidator.IsPeselValid("123abc456de").Should().BeFalse();
+        _sut.IsPeselValid("123abc456de").Should().BeFalse();
     
     [Test]
-    public void BankAccountIsValidWithTwentySixNumbers() => EmployeeModelValidator.IsBankAccountNumberValid("12345678901123456789012345").Should().BeTrue();
+    public void BankAccountIsValidWithTwentySixNumbers() => _sut.IsBankAccountNumberValid("12345678901123456789012345").Should().BeTrue();
     
     [Test]
-    public void BankAccountIsInvalidWithoutTwentySixCharacters() => EmployeeModelValidator.IsBankAccountNumberValid("123123123").Should().BeFalse();
+    public void BankAccountIsInvalidWithoutTwentySixCharacters() => _sut.IsBankAccountNumberValid("123123123").Should().BeFalse();
 
     [Test]
-    public void BankAccountIsInvalidWithDifferentCharactersThanNumbers() => EmployeeModelValidator
+    public void BankAccountIsInvalidWithDifferentCharactersThanNumbers() => _sut
         .IsBankAccountNumberValid("123A567B901D234C67G90X23T5").Should().BeFalse();
 
     [Test]
     public void EmailAddressIsValidWithCorrectSyntax() =>
-        EmployeeModelValidator.IsEmailAddressValid("pjatk@edu.pl").Should().BeTrue();
+        _sut.IsEmailAddressValid("pjatk@edu.pl").Should().BeTrue();
 
     [Test]
     public void EmailAdressIsInvalidWithPlainText() =>
-        EmployeeModelValidator.IsEmailAddressValid("pjatkedupl").Should().BeFalse();
+        _sut.IsEmailAddressValid("pjatkedupl").Should().BeFalse();
 }
