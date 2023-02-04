3ea20f712f44e7deb0d666bdba3faa6371114018
wanda96 <wanda.bojanowska@postdata.pl>
2023-02-04 14:00:38 +0100

    Poprawienie wszystkich komentarzy do wcześniejszego commita

--- Pirsoft.Frontend/src/components/employee/fields/BankAccountNumber.js
+++ Pirsoft.Frontend/src/components/employee/fields/BankAccountNumber.js
@@ -1,9 +1,7 @@
+import TextInputEmployee from "./models/TextInputEmployee";
 
 const BankAccountNumber = ({onChange, value}) => {
-
-    return <input className={"grow border text-black rounded-md text-left"} type={"text"}
-               onChange={(e) => onChange(e.target.value)} value={value}></input>
-
+    return <TextInputEmployee onChange={onChange} value={value}/>
 }
 
 export default BankAccountNumber;
--- Pirsoft.Frontend/src/components/employee/fields/FirstName.js
+++ Pirsoft.Frontend/src/components/employee/fields/FirstName.js
@@ -1,9 +1,7 @@
+import TextInputEmployee from "./models/TextInputEmployee";
 
 const FirstName = ({onChange, value}) => {
-
-    return <input className={"grow border text-black rounded-md text-left "} type={"text"}
-               onChange={(e) => onChange(e.target.value)} value={value}></input>
-
+    return <TextInputEmployee onChange={onChange} value={value}/>
 }
 
 export default FirstName;
