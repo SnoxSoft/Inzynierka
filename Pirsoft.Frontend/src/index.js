7ab36bff358f8a5c5ce08a0dd3c8b3851ec6139c
Fancia666 <s18425@pjwstk.edu.pl>
2023-02-04 14:50:04 +0100

    Usunięcie zbędnego kodu z index.js

--- Pirsoft.Frontend/src/index.js
+++ Pirsoft.Frontend/src/index.js
@@ -11,29 +11,6 @@ import Employees from "./pages/Employees";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function resizeToMinimum(){
-    console.log("ddddddd")
-    const minimum = [800, 600];
-    const current = [window.outerWidth, window.outerHeight];
-    console.log(window.innerWidth);
-    console.log(window.innerHeight);
-
-    const restricted = [];
-    let i = 2;
-
-    while(i-- > 0){
-        restricted[i] = minimum[i] > current[i] ? minimum[i] : current[i];
-    }
-
-    window.resizeTo(800,600);
-    window.innerWidth = 600;
-    window.innerHeight = 800;
-
-}
-
-window.addEventListener('resize', resizeToMinimum, false)
-
-
 root.render(
     <Provider store={store}>
         <BrowserRouter>
