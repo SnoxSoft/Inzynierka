16da4ebb6632095e34f895be8290f01a048a1f69
Fancia666 <s18425@pjwstk.edu.pl>
2023-02-13 23:03:23 +0100

    Tworzenie jsona z cechami i dopisanie fetch dla guzika ZATWIERDŹ
    Poprawienie tej funkcjonalności zostawiam na moment, w którym praca będzie opierała się na rzeczywistych endpointach i backendzie

--- Pirsoft.Frontend/src/components/employee/SkillsChangeFrame.js
+++ Pirsoft.Frontend/src/components/employee/SkillsChangeFrame.js
@@ -25,7 +25,7 @@ function SkillChangeFrame() {
             }
         }
         details.push(
-        <div className={"grid grid-cols-2 gap-4 p-4 h-9"}>
+        <div key={"skill"+availableSkill} className={"grid grid-cols-2 gap-4 p-4 h-9"}>
             <p>{allSkills[availableSkill]}</p><input className={"bg-weekend checked:bg-weekend"} type={"checkbox"} defaultChecked={hasSkill}/>
         </div>
             );
@@ -34,25 +34,42 @@ function SkillChangeFrame() {
     const saveSkills = () => {
         const element = document.getElementById('skills-edit');
         const elements = element.getElementsByTagName("div");
-        console.log();
+
+        let skillsList = [];
         try {
+            for(const ele in elements){
 
+                const p = elements[ele].getElementsByTagName("p")[0];
+                const input = elements[ele].getElementsByTagName("input")[0];
 
-        for(const ele in elements){
-            //console.log(elements[ele])
-            const p = elements[ele].getElementsByTagName("p")[0];
-            const input = elements[ele].getElementsByTagName("input")[0];
-
-            if(p !== undefined && input !== undefined){
-                console.log(p.textContent)
-                console.log(input.checked)
-                //tutaj muszę zbudować jsona dla api
+                if(p !== undefined && input !== undefined && input.checked){
+                    skillsList.push(p.textContent+"");
+                }
             }
-        }
         }catch (e) {
-
         }
+        const myObjStr = JSON.stringify(skillsList);
+        //console.log(myObjStr);
 
+        //tutaj muszę zbudować jsona dla api do edycji umiejętności
+        // edycja usuwa wszystkie skille i zapisuje na nowo umiejętności, najlepsze rozwiązanie
+        fetch("http://127.0.0.1:3001/editSkills/"+id, {
+                    method: 'PATCH',
+                    body: JSON.stringify({
+                        title: "ABC",
+                        body: myObjStr,
+                        userId: Math.random().toString(36).slice(2),
+                    }),
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    },
+                })
+                    .then((response) => response.json())
+                    .then((response) => {console.log(response)
+                    })
+                    .catch((err) => {
+                        console.log(err.message);
+                    })
     }
 
     const[wantedHeightsForList, setWantedHeightForList] = useState(0);
