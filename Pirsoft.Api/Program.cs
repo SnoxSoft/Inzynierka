e9c8db7a225da297377499cdc8657371014b39f1
giedrys-michal <giedrys.michal@gmail.com>
2023-02-22 21:56:21 +0100

    Refaktoryzacja klasy startowej projektu API

--- Pirsoft.Api/Program.cs
+++ Pirsoft.Api/Program.cs
@@ -1,37 +1,8 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web;
-using Pirsoft.Api.Interfaces;
-using Pirsoft.Api.Interfaces.Validators;
-using Pirsoft.Api.Models;
-using Pirsoft.Api.Validators;
+using Pirsoft.Api.Configurators;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+AppServicesConfigurator.Instance.Init(builder);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IEmployeeModelValidator, EmployeeModelValidator>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+AppRunner.Instance.Init(builder);
+AppRunner.Instance.Run();
