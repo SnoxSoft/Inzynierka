0aae4fa8c09d0facc7128f93d8c59fe914e4ff2b
giedrys-michal <giedrys.michal@gmail.com>
2023-02-22 21:55:54 +0100

    Utworzenie konfiguratora obiektu aplikacji

---
+++ Pirsoft.Api/Configurators/AppRunner.cs
@@ -0,0 +1,33 @@
+﻿using Pirsoft.Api.PatternsAbstraction;
+
+namespace Pirsoft.Api.Configurators
+{
+    public class AppRunner : SingletonBase<AppRunner>
+    {
+        private WebApplication _app = null!;
+
+        public void Init(WebApplicationBuilder builder)
+        {
+            _app = builder.Build();
+
+            configureAppBuild();
+        }
+
+        public void Run() => _app.Run();
+
+        private void configureAppBuild()
+        {
+            // Configure the HTTP request pipeline.
+            if (_app.Environment.IsDevelopment())
+            {
+                _app.UseSwagger();
+                _app.UseSwaggerUI();
+            }
+
+            _app.UseHttpsRedirection();
+            _app.UseAuthentication();
+            _app.UseAuthorization();
+            _app.MapControllers();
+        }
+    }
+}
