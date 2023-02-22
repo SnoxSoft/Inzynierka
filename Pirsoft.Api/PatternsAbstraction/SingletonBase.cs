abb81ff10742310796076e28d444395c3d9abbdb
giedrys-michal <giedrys.michal@gmail.com>
2023-02-22 21:54:21 +0100

    Utworzenie klasy bazowej wzorca Singleton

---
+++ Pirsoft.Api/PatternsAbstraction/SingletonBase.cs
@@ -0,0 +1,27 @@
+﻿namespace Pirsoft.Api.PatternsAbstraction
+{
+    public abstract class SingletonBase<T> : ISingleton<T>
+        where T : SingletonBase<T>, new()
+    {
+        private static T _instance = null!;
+
+        protected SingletonBase() { }
+
+        private static bool _initialised => _instance != null;
+
+        public static T Instance => _initialised
+            ? (_instance = InstanceCache.cachedInstance)
+            : _instance = new T();
+
+        class InstanceCache
+        {
+            internal static readonly T cachedInstance = _instance;
+            static InstanceCache() { }
+        }
+    }
+
+    public interface ISingleton<T>
+    {
+        static T Instance { get; } = default!;
+    }
+}
