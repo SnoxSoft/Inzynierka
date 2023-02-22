1b3dee42f63326cef6c505cc2812f5213a112140
Fancia666 <s18425@pjwstk.edu.pl>
2023-02-22 21:55:20 +0100

    Edycja list filtrujących

--- Pirsoft.Frontend/src/components/employees/search/fields/PositionsList.js
+++ Pirsoft.Frontend/src/components/employees/search/fields/PositionsList.js
@@ -4,30 +4,41 @@ import Select from 'react-select'
 const PositionsList = ({onChange}) => {
 
     // wartosci z endpointa - brak
-    const positions = [
-        { value: '', label: 'Wybierz...' },
-        { value: 'KSIĘGOWA', label: 'KSIĘGOWA' },
-        { value: 'TESTER OPROGRAMOWANIA', label: 'TESTER OPROGRAMOWANIA' },
-        { value: 'PROGRAMISTA', label: 'PROGRAMISTA' }
-    ]
+    // const positions = [
+    //     { value: '', label: 'Wybierz...' },
+    //     { value: 'KSIĘGOWA', label: 'KSIĘGOWA' },
+    //     { value: 'TESTER OPROGRAMOWANIA', label: 'TESTER OPROGRAMOWANIA' },
+    //     { value: 'PROGRAMISTA', label: 'PROGRAMISTA' }
+    // ]
 
-    //endpoint zakomentowany do uzycia
-    // const [positions, setPositions] = useState(Object);
-    //
-    // if (positions[0] === undefined) {
-    //     fetch("http://127.0.0.1:3001/getAllPositions")
-    //         .then((response) => response.json())
-    //         .then((response) => {
-    //             //console.log("fffffff "+ response[0])
-    //             setPositions(response)
-    //         })
-    //         .catch((err) => {
-    //             console.log(err.message);
-    //         })
-    // }
+    // endpoint zakomentowany do uzycia
+    const [positions, setPositions] = useState(Object);
+    function GetSortOrder(prop) {
+        return function(a, b) {
+            if (a[prop] > b[prop]) {
+                return 1;
+            } else if (a[prop] < b[prop]) {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    if (positions[0] === undefined) {
+        fetch("http://127.0.0.1:3001/getAllPositions")
+            .then((response) => response.json())
+            .then((response) => {
+                response.push({ value: '', label: 'Wybierz...' })
+                response.sort(GetSortOrder("value"))
+                setPositions(response)
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }
 
     return <Select className={"w-96 text-black"}
-                   defaultValue={positions[0]}
+                   defaultValue={{ value: '', label: 'Wybierz...' }}
                    options={positions}
                    onChange={(e) => onChange(e.value)}/>
 
--- Pirsoft.Frontend/src/components/employees/search/fields/TeamsList.js
+++ Pirsoft.Frontend/src/components/employees/search/fields/TeamsList.js
@@ -4,30 +4,43 @@ import Select from 'react-select'
 const TeamsList = ({onChange}) => {
 
     // wartosci z endpointa - brak
-    const teams = [
-        { value: '', label: 'Wybierz...' },
-        { value: 'Zespół A', label: 'zespół A' },
-        { value: 'Zespół B', label: 'zespół B' },
-        { value: 'Zespół C', label: 'zespół C' }
-    ]
+    // const teams = [
+    //     { value: '', label: 'Wybierz...' },
+    //     { value: 'Zespół A', label: 'zespół A' },
+    //     { value: 'Zespół B', label: 'zespół B' },
+    //     { value: 'Zespół C', label: 'zespół C' }
+    // ]
 
     //endpoint zakomentowany do uzycia
-    // const [teams, setTeams] = useState(Object);
-    //
-    // if (teams[0] === undefined) {
-    //     fetch("http://127.0.0.1:3001/getAllTeams")
-    //         .then((response) => response.json())
-    //         .then((response) => {
-    //             //console.log("fffffff "+ response[0])
-    //             setTeams(response)
-    //         })
-    //         .catch((err) => {
-    //             console.log(err.message);
-    //         })
-    // }
+    const [teams, setTeams] = useState(Object);
+
+    function GetSortOrder(prop) {
+        return function(a, b) {
+            if (a[prop] > b[prop]) {
+                return 1;
+            } else if (a[prop] < b[prop]) {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    if (teams[0] === undefined) {
+        fetch("http://127.0.0.1:3001/getAllTeams")
+            .then((response) => response.json())
+            .then((response) => {
+               // { value: '', label: 'Wybierz...' }
+                response.push({ value: '', label: 'Wybierz...' })
+                response.sort(GetSortOrder("value"))
+                setTeams(response)
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }
 
     return <Select className={"w-96 text-black"}
-                   defaultValue={teams[0]}
+                   defaultValue={{ value: '', label: 'Wybierz...' }}
                    options={teams}
                    onChange={(e) => onChange(e.value)}/>
 
