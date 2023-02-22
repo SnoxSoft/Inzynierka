41475aaf4cc6f9ca182125956188044b3cec5fe4
giedrys-michal <giedrys.michal@gmail.com>
2023-02-22 21:55:04 +0100

    Utworzenie konfiguratora wstrzykiwania zależności

---
+++ Pirsoft.Api/Configurators/DependencyConfigurator.cs
@@ -0,0 +1,23 @@
+﻿using Pirsoft.Api.Interfaces.Validators;
+using Pirsoft.Api.PatternsAbstraction;
+using Pirsoft.Api.Validators;
+
+namespace Pirsoft.Api.Configurators
+{
+    public class DependencyConfigurator : SingletonBase<DependencyConfigurator>
+    {
+        private IServiceCollection _services = null!;
+
+        public void Init(IServiceCollection services)
+        {
+            _services = services;
+
+            configureValidators();
+        }
+
+        private void configureValidators()
+        {
+            _services.AddScoped<IEmployeeModelValidator, EmployeeModelValidator>();
+        }
+    }
+}
