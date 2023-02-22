22a7d7961b02dafd4a4a824b3928d3f7606e14cd
giedrys-michal <giedrys.michal@gmail.com>
2023-02-22 21:55:29 +0100

    Utworzenie konfiguratora serwisów backendowych

---
+++ Pirsoft.Api/Configurators/AppServicesConfigurator.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using Pirsoft.Api.PatternsAbstraction;
+
+namespace Pirsoft.Api.Configurators
+{
+    public class AppServicesConfigurator : SingletonBase<AppServicesConfigurator>
+    {
+        private WebApplicationBuilder _builder = null!;
+
+        public void Init(WebApplicationBuilder builder)
+        {
+            _builder = builder;
+
+            configureServices();
+
+            DependencyConfigurator.Instance.Init(_builder.Services);
+        }
+
+        private void configureServices()
+        {
+            _builder.Services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(_builder.Configuration.GetSection("AzureAd"));
+            _builder.Services
+                .AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            _builder.Services
+                .AddEndpointsApiExplorer();
+            _builder.Services
+                .AddSwaggerGen();
+        }
+    }
+}
