876a1df661f672c096070a8d3c68e191a346380b
Fancia666 <s18425@pjwstk.edu.pl>
2023-03-19 16:43:34 +0100

    Dodanie "/" do linków w menu po lewej stronie

--- Pirsoft.Frontend/src/components/base/LeftMenu.js
+++ Pirsoft.Frontend/src/components/base/LeftMenu.js
@@ -8,9 +8,9 @@ function LeftMenu(){
                 <>
             <MenuButton link={"/employees"} value={"PRACOWNICY"}/>
             <MenuButton link={'/employee/-1'} value={"REJESTRACJA"}/>
-            <MenuButton link={"schedule"} value={"HARMONOGRAM OSOBISTY"}/>
-            <MenuButton link={"company-schedule"} value={"HARMONOGRAM FIRMY"}/>
-            <MenuButton link={"teams"} value={"ZESPOŁY W FIRMIE"}/>
+            <MenuButton link={"/schedule"} value={"HARMONOGRAM OSOBISTY"}/>
+            <MenuButton link={"/company-schedule"} value={"HARMONOGRAM FIRMY"}/>
+            <MenuButton link={"/teams"} value={"ZESPOŁY W FIRMIE"}/>
             <MenuButton link={"/absences"} value={"MOJE NIEOBECNOSCI"}/>
             <MenuButton link={"/requests"} value={"WNIOSKI PRACOWNIKOW"}/>
                     </>
