09883ac64087caca804cc29bececaef610aa8b15
Fancia666 <s18425@pjwstk.edu.pl>
2023-03-19 16:04:31 +0100

    Ustawianie daty dzisiejszej gdy ustawiona została data nieprawidłowa

--- Pirsoft.Frontend/src/components/absences/Calendar.js
+++ Pirsoft.Frontend/src/components/absences/Calendar.js
@@ -1,5 +1,41 @@
 import React, {useState, useRef} from "react";
+import companySchedule from "../../pages/CompanySchedule";
 const Calendar= ({setDateTo,setDateFrom,from,to, disabled = false}) => {
+
+    const options = {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit"
+    }
+
+    function validateDate(pickedDateText, method){
+        let pickedDate = new Date(pickedDateText)
+
+        if(method === "from") {
+            setDateFrom(pickedDateText)
+        }else {
+            setDateTo(pickedDateText)
+        }
+
+        if(pickedDate.getFullYear().toString().length === 4
+            && (pickedDate.getFullYear().toString().substring(0, 2) !== "19" ||
+                pickedDate.getFullYear().toString().substring(0, 2) !== "20")){
+            if(method === "from") {
+                setDateFrom(new Date().toLocaleDateString("sv", options))
+            }else {
+                setDateTo(new Date().toLocaleDateString("sv", options))
+            }
+        }
+
+        if(pickedDate.getFullYear().toString().length > 4 ||
+        pickedDate.getFullYear().toString().length < 4){
+            if(method === "from") {
+                setDateFrom(new Date().toLocaleDateString("sv", options))
+            }else {
+                setDateTo(new Date().toLocaleDateString("sv", options))
+            }
+        }
+    }
 
     return (
         <div className={"content-center"}>
@@ -12,8 +48,9 @@ const Calendar= ({setDateTo,setDateFrom,from,to, disabled = false}) => {
                     required
                     pattern="[0-9]{4}-[0-9]{2}"
                     onChange={(e) => {
-                        setDateFrom(e.target.value);
+                        setDateFrom(e.target.value)
                     }}
+                    onBlur={() => validateDate(from, "from")}
                 /></p>
                 <p className={""}>DO: <input
                     type={"date"}
@@ -23,8 +60,9 @@ const Calendar= ({setDateTo,setDateFrom,from,to, disabled = false}) => {
                     required
                     pattern="[0-9]{4}-[0-9]{2}"
                     onChange={(e) => {
-                        setDateTo(e.target.value);
+                        setDateTo(e.target.value)
                     }}
+                    onBlur={() => validateDate(to, "to")}
                 /></p>
             </div>
         </div>
