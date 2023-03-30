d663edee6dded5e6ad6c025e9962e74cc49eeff3
20497 <s20497@pjwstk.edu.pl>
2023-03-30 20:56:06 +0200

    TEST-110 added tests for negative scenarios

--- Pirsoft.IntegrationTests/EmployeeControllerTests.cs
+++ Pirsoft.IntegrationTests/EmployeeControllerTests.cs
@@ -41,4 +41,60 @@ public class EmployeeControllerTests
         response.Result.IsSuccessStatusCode.Should().BeTrue();
         response.IsCompletedSuccessfully.Should().BeTrue();
     }
+    
+    [Test]
+    public void CreateNewEmployeeEndpointIsReturningFailCodeWithInvalidPesel()
+    {
+        var employeeModel = new
+        {
+            FirstName = "Janusz",
+            LastName = "Kowalski",
+            Email = "janusz.test@gmail.com",
+            Password = "123qwe",
+            AccountType = (AccountTypeModel)new AccountTypeCreator(EAccountType.Employee).CreateModel(), 
+            Pesel = "1",
+            BankAccountNumber = "12345678901234567890123456",
+            DepartmentId = 1,
+            SeniorityInMonths = 10,
+            EmploymentStartDate = (2020, 10, 10),
+            IsActive = Convert.ToSByte(true),
+            PasswordReset = Convert.ToSByte(false),
+            DateOfBirth = (2000, 10, 10),
+            GrossSalary = 10000d,
+            PositionType = (PositionTypeModel)new PositionTypeCreator(EPositionType.Mid).CreateModel(),
+        };
+
+        var response = _client.PostAsJsonAsync("https://localhost:7120/create/new/employee", employeeModel);
+
+        response.Result.IsSuccessStatusCode.Should().BeFalse();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+    
+    [Test]
+    public void CreateNewEmployeeEndpointIsReturningFailCodeWithInvalidBankAccountNumber()
+    {
+        var employeeModel = new
+        {
+            FirstName = "Janusz",
+            LastName = "Kowalski",
+            Email = "janusz.test@gmail.com",
+            Password = "123qwe",
+            AccountType = (AccountTypeModel)new AccountTypeCreator(EAccountType.Employee).CreateModel(), 
+            Pesel = "00112212345",
+            BankAccountNumber = "1",
+            DepartmentId = 1,
+            SeniorityInMonths = 10,
+            EmploymentStartDate = (2020, 10, 10),
+            IsActive = Convert.ToSByte(true),
+            PasswordReset = Convert.ToSByte(false),
+            DateOfBirth = (2000, 10, 10),
+            GrossSalary = 10000d,
+            PositionType = (PositionTypeModel)new PositionTypeCreator(EPositionType.Mid).CreateModel(),
+        };
+
+        var response = _client.PostAsJsonAsync("https://localhost:7120/create/new/employee", employeeModel);
+
+        response.Result.IsSuccessStatusCode.Should().BeFalse();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
 }
