65224b9a92138cdbb762b689c9b7b6d5b0a3b3d9
giedrys-michal <giedrys.michal@gmail.com>
2023-04-03 22:51:19 +0200

    Pirsoft.Api: Aktualizacja modelu EmployeeModel
    Dopasowanie nazw i typów propercji modelu do nazw i typów w bazie danych.
    Poprawnienie nazw w powiązanych klasach.

--- Pirsoft.Api/Controllers/EmployeeController.cs
+++ Pirsoft.Api/Controllers/EmployeeController.cs
@@ -16,8 +16,8 @@ public class EmployeeController : ControllerBase
     public EmployeeController(IEmployeeModelValidator validator) => _validator = validator;
 
     [HttpPost("create/new/employee")]
-    public IApiModel CreateNewEmployee(string firstName, string lastName, string email, string password, ECompanyRole companyRole, string pesel, string bankAccountNumber,
-         int departmentId, int seniorityInMonths, DateTime employmentStartDate, bool isActive, bool passwordReset, DateTime dateOfBirth, double grossSalary, ESeniorityLevel seniorityLevel)
+    public IApiModel CreateNewEmployee(string firstName, string lastName, string email, string password, string pesel, string bankAccountNumber, int departmentId, int seniorityInMonths, double grossSalary,
+        bool isActive, bool passwordReset, DateTime dateOfBirth, DateTime employmentStartDate, ECompanyRole companyRole, EContractType contractType, ESeniorityLevel seniorityLevel)
     {
         if (!_validator.IsPeselValid(pesel))
             pesel = "Missing data";
@@ -26,7 +26,7 @@ public class EmployeeController : ControllerBase
         if (!_validator.IsBankAccountNumberValid(bankAccountNumber))
             bankAccountNumber = "Missing data";
 
-        return new EmployeeCreator(firstName, lastName, email, password, companyRole, pesel, bankAccountNumber, departmentId,
-            seniorityInMonths, employmentStartDate, isActive, passwordReset, dateOfBirth, grossSalary, seniorityLevel).CreateModel();
+        return new EmployeeCreator(firstName, lastName, email, password, pesel, bankAccountNumber, departmentId, seniorityInMonths, grossSalary,
+            isActive, passwordReset, dateOfBirth, employmentStartDate, companyRole, contractType, seniorityLevel).CreateModel();
     }
 }
