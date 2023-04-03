0bbc1456a5044cb1c20a1cb86d6f6bc61da990d4
giedrys-michal <giedrys.michal@gmail.com>
2023-04-03 23:18:12 +0200

    Repo: Aktualizacja pliku zrzutu bazy danych pirsoft.sql
    Zaktualizowanie pliku zrzutu bazy danych o aktualne zmiany wprowadzone w modelu.

--- pirsoft.sql
+++ pirsoft.sql
@@ -1,13 +1,13 @@
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: pirsoft
+-- Host: localhost    Database: pirsoft
 -- ------------------------------------------------------
 -- Server version	8.0.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,212 +16,211 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `account_type`
+-- Table structure for table `absences`
 --
 
-DROP TABLE IF EXISTS `account_type`;
+DROP TABLE IF EXISTS `absences`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `account_type` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `accountType` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `roleName_UNIQUE` (`accountType`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `absences` (
+  `absence_id` int NOT NULL AUTO_INCREMENT,
+  `absence_start_date` date NOT NULL,
+  `absence_end_date` date NOT NULL,
+  `duration` int NOT NULL,
+  `absence_employee_id` int NOT NULL,
+  PRIMARY KEY (`absence_id`),
+  UNIQUE KEY `Id_UNIQUE` (`absence_id`),
+  KEY `fk_absence_employee_idx` (`absence_employee_id`),
+  CONSTRAINT `fk_absence_employee` FOREIGN KEY (`absence_employee_id`) REFERENCES `employees` (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `account_type`
+-- Dumping data for table `absences`
 --
 
-LOCK TABLES `account_type` WRITE;
-/*!40000 ALTER TABLE `account_type` DISABLE KEYS */;
-INSERT INTO `account_type` VALUES (1,'Accounting'),(2,'Administrator'),(3,'BoardMember'),(4,'Ceo'),(5,'Employee'),(6,'HumanResources'),(7,'Manager');
-/*!40000 ALTER TABLE `account_type` ENABLE KEYS */;
+LOCK TABLES `absences` WRITE;
+/*!40000 ALTER TABLE `absences` DISABLE KEYS */;
+/*!40000 ALTER TABLE `absences` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `contract_type`
+-- Table structure for table `company_roles`
 --
 
-DROP TABLE IF EXISTS `contract_type`;
+DROP TABLE IF EXISTS `company_roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `contract_type` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `contractType` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `contractName_UNIQUE` (`contractType`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `company_roles` (
+  `role_id` int NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`role_id`),
+  UNIQUE KEY `id_UNIQUE` (`role_id`),
+  UNIQUE KEY `roleName_UNIQUE` (`role_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `contract_type`
+-- Dumping data for table `company_roles`
 --
 
-LOCK TABLES `contract_type` WRITE;
-/*!40000 ALTER TABLE `contract_type` DISABLE KEYS */;
-INSERT INTO `contract_type` VALUES (1,'B2B'),(2,'ContractOfEmployment'),(3,'ContractOfMandate');
-/*!40000 ALTER TABLE `contract_type` ENABLE KEYS */;
+LOCK TABLES `company_roles` WRITE;
+/*!40000 ALTER TABLE `company_roles` DISABLE KEYS */;
+INSERT INTO `company_roles` VALUES (1,'Accounting'),(2,'Administrator'),(3,'BoardMember'),(4,'Ceo'),(5,'Employee'),(6,'HumanResources'),(7,'Manager');
+/*!40000 ALTER TABLE `company_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `department`
+-- Table structure for table `contract_types`
 --
 
-DROP TABLE IF EXISTS `department`;
+DROP TABLE IF EXISTS `contract_types`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `department` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `departmentName` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `departamentName_UNIQUE` (`departmentName`)
+CREATE TABLE `contract_types` (
+  `contract_id` int NOT NULL AUTO_INCREMENT,
+  `contract_type_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`contract_id`),
+  UNIQUE KEY `contractName_UNIQUE` (`contract_type_name`),
+  UNIQUE KEY `id_UNIQUE` (`contract_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contract_types`
+--
+
+LOCK TABLES `contract_types` WRITE;
+/*!40000 ALTER TABLE `contract_types` DISABLE KEYS */;
+INSERT INTO `contract_types` VALUES (1,'B2B'),(2,'ContractOfEmployment'),(3,'ContractOfMandate'),(4,'Unassigned');
+/*!40000 ALTER TABLE `contract_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `departments`
+--
+
+DROP TABLE IF EXISTS `departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `departments` (
+  `department_id` int NOT NULL AUTO_INCREMENT,
+  `department_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`department_id`),
+  UNIQUE KEY `id_UNIQUE` (`department_id`),
+  UNIQUE KEY `departamentName_UNIQUE` (`department_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `department`
+-- Dumping data for table `departments`
 --
 
-LOCK TABLES `department` WRITE;
-/*!40000 ALTER TABLE `department` DISABLE KEYS */;
-INSERT INTO `department` VALUES (1,'PIR-DPT-01');
-/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+INSERT INTO `departments` VALUES (1,'DPT_TEST');
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `employee`
+-- Table structure for table `employees`
 --
 
-DROP TABLE IF EXISTS `employee`;
+DROP TABLE IF EXISTS `employees`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `employee` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(50) NOT NULL,
-  `lastName` varchar(50) NOT NULL,
-  `email` varchar(120) NOT NULL,
+CREATE TABLE `employees` (
+  `employee_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email_address` varchar(120) NOT NULL,
   `password` varchar(120) NOT NULL,
   `pesel` varchar(11) NOT NULL,
-  `bankAccountNumber` varchar(26) DEFAULT NULL,
-  `seniorityInMonths` int NOT NULL,
-  `employmentStartDate` date NOT NULL,
-  `isActive` tinyint NOT NULL,
-  `passwordReset` tinyint NOT NULL,
-  `dateOfBirth` date NOT NULL,
-  `grossSalary` double NOT NULL,
-  `contract_type_id` int NOT NULL,
-  `department_id` int NOT NULL,
-  `position_id` int NOT NULL,
-  `account_type_id` int NOT NULL,
-  PRIMARY KEY (`id`,`contract_type_id`,`department_id`,`position_id`,`account_type_id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  `bank_account_number` varchar(26) DEFAULT NULL,
+  `seniority_in_months` int NOT NULL,
+  `employment_start_date` date NOT NULL,
+  `is_active` tinyint NOT NULL,
+  `password_reset` tinyint NOT NULL,
+  `birth_date` date NOT NULL,
+  `salary_gross` double NOT NULL,
+  `employee_contract_type_id` int NOT NULL,
+  `employee_department_id` int NOT NULL,
+  `employee_seniority_level_id` int NOT NULL,
+  `employee_company_role_id` int NOT NULL,
+  PRIMARY KEY (`employee_id`,`employee_contract_type_id`,`employee_department_id`,`employee_seniority_level_id`,`employee_company_role_id`),
+  UNIQUE KEY `id_UNIQUE` (`employee_id`),
   UNIQUE KEY `pesel_UNIQUE` (`pesel`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  KEY `fk_employee_position_idx` (`position_id`),
-  KEY `fk_employee_contract_type_id_idx` (`contract_type_id`),
-  KEY `fk_employee_account_type_id_idx` (`account_type_id`),
-  KEY `fk_employee_department_idx` (`department_id`),
-  CONSTRAINT `fk_employee_account_type_id` FOREIGN KEY (`account_type_id`) REFERENCES `account_type` (`id`),
-  CONSTRAINT `fk_employee_contract_type_id` FOREIGN KEY (`contract_type_id`) REFERENCES `contract_type` (`id`),
-  CONSTRAINT `fk_employee_department` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`),
-  CONSTRAINT `fk_employee_position` FOREIGN KEY (`position_id`) REFERENCES `position` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+  UNIQUE KEY `email_UNIQUE` (`email_address`),
+  KEY `fk_employee_company_role_idx` (`employee_company_role_id`),
+  KEY `fk_employee_contract_type_idx` (`employee_contract_type_id`),
+  KEY `fk_employee_department_idx` (`employee_department_id`),
+  KEY `fk_employee_seniority_level_idx` (`employee_seniority_level_id`),
+  CONSTRAINT `fk_employee_company_role` FOREIGN KEY (`employee_company_role_id`) REFERENCES `company_roles` (`role_id`),
+  CONSTRAINT `fk_employee_contract_type` FOREIGN KEY (`employee_contract_type_id`) REFERENCES `contract_types` (`contract_id`),
+  CONSTRAINT `fk_employee_department` FOREIGN KEY (`employee_department_id`) REFERENCES `departments` (`department_id`),
+  CONSTRAINT `fk_employee_seniority_level` FOREIGN KEY (`employee_seniority_level_id`) REFERENCES `seniority_levels` (`seniority_level_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `employee`
+-- Dumping data for table `employees`
 --
 
-LOCK TABLES `employee` WRITE;
-/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `employee_has_skills`
+-- Table structure for table `employees_skills`
 --
 
-DROP TABLE IF EXISTS `employee_has_skills`;
+DROP TABLE IF EXISTS `employees_skills`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `employee_has_skills` (
+CREATE TABLE `employees_skills` (
   `employee_id` int NOT NULL,
-  `skills_id` int NOT NULL,
-  PRIMARY KEY (`employee_id`,`skills_id`),
+  `skill_id` int NOT NULL,
   KEY `fk_employee_has_skills_employee1_idx` (`employee_id`),
-  KEY `fk_employee_has_skills_skills_idx` (`skills_id`),
-  CONSTRAINT `fk_employee_has_skills_employee` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`),
-  CONSTRAINT `fk_employee_has_skills_skills` FOREIGN KEY (`skills_id`) REFERENCES `skills` (`id`)
+  KEY `fk_employees_skills_skill_idx` (`skill_id`),
+  CONSTRAINT `fk_employees_skills_employee` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`),
+  CONSTRAINT `fk_employees_skills_skill` FOREIGN KEY (`skill_id`) REFERENCES `skills` (`skill_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `employee_has_skills`
+-- Dumping data for table `employees_skills`
 --
 
-LOCK TABLES `employee_has_skills` WRITE;
-/*!40000 ALTER TABLE `employee_has_skills` DISABLE KEYS */;
-/*!40000 ALTER TABLE `employee_has_skills` ENABLE KEYS */;
+LOCK TABLES `employees_skills` WRITE;
+/*!40000 ALTER TABLE `employees_skills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employees_skills` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `holiday`
+-- Table structure for table `seniority_levels`
 --
 
-DROP TABLE IF EXISTS `holiday`;
+DROP TABLE IF EXISTS `seniority_levels`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `holiday` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `holidayStart` date NOT NULL,
-  `holidayEnd` date NOT NULL,
-  `duration` int NOT NULL,
-  `employee_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `Id_UNIQUE` (`id`),
-  KEY `fk_holiday_employee_id_idx` (`employee_id`),
-  CONSTRAINT `fk_holiday_employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `seniority_levels` (
+  `seniority_level_id` int NOT NULL AUTO_INCREMENT,
+  `seniority_level_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`seniority_level_id`),
+  UNIQUE KEY `position_UNIQUE` (`seniority_level_name`),
+  UNIQUE KEY `id_UNIQUE` (`seniority_level_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `holiday`
+-- Dumping data for table `seniority_levels`
 --
 
-LOCK TABLES `holiday` WRITE;
-/*!40000 ALTER TABLE `holiday` DISABLE KEYS */;
-/*!40000 ALTER TABLE `holiday` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `position`
---
-
-DROP TABLE IF EXISTS `position`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `position` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `positionType` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `position_UNIQUE` (`positionType`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `position`
---
-
-LOCK TABLES `position` WRITE;
-/*!40000 ALTER TABLE `position` DISABLE KEYS */;
-INSERT INTO `position` VALUES (1,'Junior'),(2,'Mid'),(3,'Senior');
-/*!40000 ALTER TABLE `position` ENABLE KEYS */;
+LOCK TABLES `seniority_levels` WRITE;
+/*!40000 ALTER TABLE `seniority_levels` DISABLE KEYS */;
+INSERT INTO `seniority_levels` VALUES (1,'Junior'),(2,'Mid'),(3,'Other'),(4,'Senior');
+/*!40000 ALTER TABLE `seniority_levels` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -232,12 +231,12 @@ DROP TABLE IF EXISTS `skills`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `skills` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `skillName` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `skillName_UNIQUE` (`skillName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `skill_id` int NOT NULL AUTO_INCREMENT,
+  `skill_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`skill_id`),
+  UNIQUE KEY `id_UNIQUE` (`skill_id`),
+  UNIQUE KEY `skillName_UNIQUE` (`skill_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -246,6 +245,7 @@ CREATE TABLE `skills` (
 
 LOCK TABLES `skills` WRITE;
 /*!40000 ALTER TABLE `skills` DISABLE KEYS */;
+INSERT INTO `skills` VALUES (5,'Html'),(2,'Java'),(4,'MsOffice'),(3,'Php'),(1,'Sql');
 /*!40000 ALTER TABLE `skills` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -258,4 +258,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-11 21:40:15
+-- Dump completed on 2023-04-03 23:05:53
