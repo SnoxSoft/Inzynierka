04fdf13ae5e3e91eeb5dce7596dfed9aff2056bb
giedrys-michal <giedrys.michal@gmail.com>
2023-04-15 22:52:04 +0200

    Pirsoft.Api/UT: Przygotowanie testów dla klasy ContractTypeEntityBuilder

---
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/ContractTypeEntityBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
+{
+    public class ContractTypeEntityBuilder : EntityBuilderBase<ContractTypeModel>
+    {
+        public ContractTypeEntityBuilder(ModelBuilder modelBuilder) : base(modelBuilder) { }
+
+        public override ModelBuilder Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
