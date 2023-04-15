7c90f87c0eb185c6f20a1f4c66fe2eb55a4d103f
giedrys-michal <giedrys.michal@gmail.com>
2023-04-15 23:41:35 +0200

    Pirsoft.Api: Implementacja klasy SkillEntityBuilder zgodnie z testami

--- Pirsoft.Api/DatabaseManagement/EntityBuilders/SkillEntityBuilder.cs
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/SkillEntityBuilder.cs
@@ -8,8 +8,12 @@ namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
         public SkillEntityBuilder(ModelBuilder modelBuilder) : base(modelBuilder) { }
 
         public override ModelBuilder Build()
-        {
-            throw new NotImplementedException();
-        }
+            => _modelBuilder.Entity<SkillModel>(entity =>
+            {
+                entity.HasKey(e => e.skill_id).HasName("PRIMARY");
+                entity.HasIndex(e => e.skill_id, "id_UNIQUE").IsUnique();
+                entity.HasIndex(e => e.skill_name, "skillName_UNIQUE").IsUnique();
+                entity.Property(e => e.skill_name).HasMaxLength(45);
+            });
     }
 }
