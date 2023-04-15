8399cf3edebe7c2467e993844bfc42f7ccf1015a
giedrys-michal <giedrys.michal@gmail.com>
2023-04-15 21:25:14 +0200

    Pirsoft.Api: Implementacja metod zgodnie z testami

--- Pirsoft.Api/DatabaseManagement/EntityBuilders/AbsenceEntityBuilder.cs
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/AbsenceEntityBuilder.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Pirsoft.Api.Models;
 
 namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
 {
@@ -9,13 +11,33 @@ namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
         public AbsenceEntityBuilder(ModelBuilder modelBuilder) => _modelBuilder = modelBuilder;
 
         public ModelBuilder Build()
-        {
-            throw new NotImplementedException();
-        }
+            => _modelBuilder.Entity<AbsenceModel>(entity =>
+            {
+                entity.HasKey(e => e.absence_id).HasName("PRIMARY");
 
-        public string GetEntityPrimaryKey(ModelBuilder modelBuilder)
+                entity.HasIndex(e => e.absence_id, "Id_UNIQUE").IsUnique();
+
+                entity.HasIndex(e => e.absence_employee_id, "fk_absence_employee_idx");
+
+                entity.Property(e => e.absence_end_date).HasColumnType("date");
+                entity.Property(e => e.absence_start_date).HasColumnType("date");
+
+                entity.HasOne(d => d.absence_employee).WithMany(p => p.absences)
+                    .HasPrincipalKey(p => p.employee_id)
+                    .HasForeignKey(d => d.absence_employee_id)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_absence_employee");
+            });
+
+        public string GetEntityPrimaryKey(ModelBuilder builder)
         {
-            throw new NotImplementedException();
+            IMutableEntityType? matchingEntity = builder.Model.GetEntityTypes()
+                .Where(e => e.Name.Equals(typeof(AbsenceModel).FullName))
+                .FirstOrDefault();
+
+            string primaryKey = (matchingEntity?.FindPrimaryKey()?.Properties.FirstOrDefault()?.Name) ?? string.Empty;
+
+            return primaryKey;
         }
     }
 
