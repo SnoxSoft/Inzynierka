ab1234a5d9249f57d82a9444431aa6c7adaca0c8
giedrys-michal <giedrys.michal@gmail.com>
2023-04-15 23:30:01 +0200

    Pirsoft.Api/UT: Przygotowanie testów dla klasy SeniorityLevelEntityBuilder

---
+++ Pirsoft.UnitTests/DatabaseManagement/EntityBuilders/SeniorityLevelEntityBuilderTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Pirsoft.Api.DatabaseManagement.EntityBuilders;
+
+namespace Pirsoft.UnitTests.DatabaseManagement.EntityBuilders
+{
+    public class SeniorityLevelEntityBuilderTests
+    {
+        private ModelBuilder _fakeModelBuilder = null!;
+
+        [SetUp]
+        public void SetUp() => _fakeModelBuilder = new();
+
+        [Test]
+        public void Build_ReturnsUpdatedModelBuilder()
+        {
+            //Arrange
+            SeniorityLevelEntityBuilder sut = new(_fakeModelBuilder);
+
+            //Act
+            ModelBuilder result = sut.Build();
+
+            //Assert
+            result.Should().BeEquivalentTo(_fakeModelBuilder);
+        }
+
+        [Test]
+        public void GetEntityPrimaryKey_ReturnsCorrectPrimaryKey()
+        {
+            //Arrange
+            string expectedPrimaryKeyName = "seniority_level_id";
+            SeniorityLevelEntityBuilder sut = new(_fakeModelBuilder);
+
+            //Act
+            string result = sut.GetEntityPrimaryKey(sut.Build());
+
+            //Assert
+            result.Should().Be(expectedPrimaryKeyName);
+        }
+    }
+}
