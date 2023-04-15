d42cd10e174c64911490c97a1c640ccc6288d58f
giedrys-michal <giedrys.michal@gmail.com>
2023-04-15 23:00:49 +0200

    Pirsoft.Api/UT: Przygotowanie testów dla klasy DepartmentEntityBuilder

---
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/DepartmentEntityBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
+{
+    public class DepartmentEntityBuilder : EntityBuilderBase<DepartmentModel>
+    {
+        public DepartmentEntityBuilder(ModelBuilder modelBuilder) : base(modelBuilder) { }
+
+        public override ModelBuilder Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
