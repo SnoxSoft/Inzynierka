5cebbc42ff27bf28f6760a8f1e801ddb4792ec84
giedrys-michal <giedrys.michal@gmail.com>
2023-04-15 23:20:01 +0200

    Pirsoft.Api/UT: Przygotowanie testów dla klasy EmployeeEntityBuilder

---
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/EmployeeEntityBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
+{
+    public class EmployeeEntityBuilder : EntityBuilderBase<EmployeeModel>
+    {
+        public EmployeeEntityBuilder(ModelBuilder modelBuilder) : base(modelBuilder) { }
+
+        public override ModelBuilder Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
