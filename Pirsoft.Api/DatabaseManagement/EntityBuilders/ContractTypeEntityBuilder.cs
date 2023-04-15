fcfdc1f3a98a55d072e5c5eba550f2c479dbeac5
giedrys-michal <giedrys.michal@gmail.com>
2023-04-15 22:54:52 +0200

    Pirsoft.Api: Implementacja klasy ContractTypeEntityBuilder zgodnie z testami

--- Pirsoft.Api/DatabaseManagement/EntityBuilders/ContractTypeEntityBuilder.cs
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/ContractTypeEntityBuilder.cs
@@ -8,8 +8,12 @@ namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
         public ContractTypeEntityBuilder(ModelBuilder modelBuilder) : base(modelBuilder) { }
 
         public override ModelBuilder Build()
-        {
-            throw new NotImplementedException();
-        }
+            => _modelBuilder.Entity<ContractTypeModel>(entity =>
+            {
+                entity.HasKey(e => e.contract_id).HasName("PRIMARY");
+                entity.HasIndex(e => e.contract_type_name, "contractName_UNIQUE").IsUnique();
+                entity.HasIndex(e => e.contract_id, "id_UNIQUE").IsUnique();
+                entity.Property(e => e.contract_type_name).HasMaxLength(45);
+            });
     }
 }
