a1769ab39c16d5bba4091f33088d264d6e0691ea
giedrys-michal <giedrys.michal@gmail.com>
2023-04-15 22:42:22 +0200

    Pirsoft.Api/UT: Przygotowanie testów dla klasy CompanyRoleEntityBuilder

---
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/CompanyRoleEntityBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
+{
+    public class CompanyRoleEntityBuilder : EntityBuilderBase<CompanyRoleModel>
+    {
+        public CompanyRoleEntityBuilder(ModelBuilder modelBuilder) : base(modelBuilder) { }
+
+        public override ModelBuilder Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
