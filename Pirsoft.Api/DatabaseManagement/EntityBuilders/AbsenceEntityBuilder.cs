2fe5f991765a5e029e6da750ad132a8cc62bbe80
giedrys-michal <giedrys.michal@gmail.com>
2023-04-15 15:06:34 +0200

    Pirsoft.Api/UT: Przygotowanie testów dla klasy AbsenceEntityBuilder
    Utworzenie klasy testów jednostkowych dla klasy AbsenceEntityBuilder.
    Przygotowanie pustych metod klasy AbsenceEntityBuilder rzucających wyjątki NotImplementedException.

---
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/AbsenceEntityBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
+{
+    public class AbsenceEntityBuilder : IEntityBuilder
+    {
+        private readonly ModelBuilder _modelBuilder = null!;
+
+        public AbsenceEntityBuilder(ModelBuilder modelBuilder) => _modelBuilder = modelBuilder;
+
+        public ModelBuilder Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEntityPrimaryKey(ModelBuilder modelBuilder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IEntityBuilder
+    {
+        ModelBuilder Build();
+        string GetEntityPrimaryKey(ModelBuilder modelBuilder);
+    }
+}
