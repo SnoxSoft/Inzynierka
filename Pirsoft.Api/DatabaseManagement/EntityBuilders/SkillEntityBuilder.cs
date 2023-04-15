8c9a61e8e23f25f22694ea658b0bcce4311a0560
giedrys-michal <giedrys.michal@gmail.com>
2023-04-15 23:38:59 +0200

    Pirsoft.Api/UT: Przygotowanie testów dla klasy SkillEntityBuilder

---
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/SkillEntityBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
+{
+    public class SkillEntityBuilder : EntityBuilderBase<SkillModel>
+    {
+        public SkillEntityBuilder(ModelBuilder modelBuilder) : base(modelBuilder) { }
+
+        public override ModelBuilder Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
