36bc42440766f7ba5c52c7be0bfab33a154d871e
giedrys-michal <giedrys.michal@gmail.com>
2023-04-17 10:01:11 +0200

    Pirsoft.Api/UT: Przygotowanie testów dla klasy DatabaseModelBuilder

---
+++ Pirsoft.Api/DatabaseManagement/DatabaseModelBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pirsoft.Api.DatabaseManagement
+{
+    public class DatabaseModelBuilder : IDatabaseModelBuilder
+    {
+        public ModelBuilder BuildModel(ModelBuilder modelBuilder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IDatabaseModelBuilder
+    {
+        ModelBuilder BuildModel(ModelBuilder modelBuilder);
+    }
+}
