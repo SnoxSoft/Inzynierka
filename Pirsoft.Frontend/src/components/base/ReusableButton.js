ca3db2ed92910f459d90a7f181dc21b84004d5b0
Fancia666 <s18425@pjwstk.edu.pl>
2023-04-18 23:03:23 +0200

    Harmonogram Osobisty - Schedule + modyfikacja ReusableButton
    Zmiany dotyczące wyświetlania i formatowania wszystkich guzików, dodanie/poprawienie ID w Harmonogramie osobistym

--- Pirsoft.Frontend/src/components/base/ReusableButton.js
+++ Pirsoft.Frontend/src/components/base/ReusableButton.js
@@ -1,11 +1,17 @@
 import {Link} from "react-router-dom";
 
-const ReusableButton = ({value, link, onClick, color = "bg-brown-menu", formatting = "border-2 border-b-workday min-w-min w-24 h-12"}) => {
+const ReusableButton = ({value, link, onClick, color = "bg-brown-menu", formatting = "border-2 border-b-workday min-w-min w-24 h-12", id = ""}) => {
+    let buttonFormat = typeof value === 'string'?
+        "grid grid-cols-1 place-content-center justify-self-center " :
+        "grid grid-rows-1 place-content-center place-items-center "
+
     return <div
             className={color + " " + formatting + " rounded-md grid grid-rows-1 place-items-center text-workday font-bold text-center hover:cursor-pointer hover:bg-gray-600 "}
             onClick={onClick}>
-            <Link to={link}
-                  className={""}>{value}</Link>
+            <Link id={id}
+                to={link}
+                  className={ formatting !== "" ? " border-0 w-full h-full " + buttonFormat : ""}
+            >{value}</Link>
         </div>
 }
 
--- Pirsoft.Frontend/src/components/schedule/ScheduleListItem.js
+++ Pirsoft.Frontend/src/components/schedule/ScheduleListItem.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {HiArrowLeft} from "react-icons/hi";
 
-const ScheduleListItem = ({text, date, loadWholeMonthData}) => {
+const ScheduleListItem = ({text, date, loadWholeMonthData, listId}) => {
     const[showHideButtons, setShowHideButtons] = useState(false);
 
     const showOptions = () => {
@@ -15,7 +15,8 @@ const ScheduleListItem = ({text, date, loadWholeMonthData}) => {
         loadWholeMonthData({text: text, date: date})
     }
 
-    return <li className={"flex flex-row m-4 p-4 gap-4 text-workday hover:cursor-pointer rounded-md max-w-fit"}
+    return <li id={"schedule-list-item-"+listId}
+        className={"flex flex-row m-4 p-4 gap-4 text-workday hover:cursor-pointer rounded-md max-w-fit"}
             onMouseOver={showOptions} onMouseLeave={hideOptions}
                onClick={() => openWholeMonth()}>
                 <div className={"grow-0"}>
