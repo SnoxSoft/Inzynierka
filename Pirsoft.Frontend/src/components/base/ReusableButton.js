f76585cee5df4de21e51b69210a0c46193fcb92d
Fancia666 <s18425@pjwstk.edu.pl>
2023-04-19 19:53:57 +0200

    Poprawa funkcjonowania guzika ReusableButton

--- Pirsoft.Frontend/src/components/base/ReusableButton.js
+++ Pirsoft.Frontend/src/components/base/ReusableButton.js
@@ -1,18 +1,20 @@
 import {Link} from "react-router-dom";
 
 const ReusableButton = ({value, link, onClick, color = "bg-brown-menu", formatting = "border-2 border-b-workday min-w-min w-24 h-12", id = ""}) => {
-    let buttonFormat = typeof value === 'string'?
-        "grid grid-cols-1 place-content-center justify-self-center " :
-        "grid grid-rows-1 place-content-center place-items-center "
 
-    return <div
-            className={color + " " + formatting + " rounded-md grid grid-rows-1 place-items-center text-workday font-bold text-center hover:cursor-pointer hover:bg-gray-600 "}
-            onClick={onClick}>
+    // let buttonFormat = typeof value === 'string'?
+    //     "grid grid-cols-1 place-content-center justify-self-center " :
+    //     "grid grid-rows-1 place-content-center place-items-center "
+
+    return <>
             <Link id={id}
-                to={link}
-                  className={ formatting !== "" ? " border-0 w-full h-full " + buttonFormat : ""}
+                  onClick={onClick}
+                  to={link}
+                  className={
+                      color + " " + formatting + " rounded-md grid grid-rows-1 place-items-center text-workday font-bold text-center hover:cursor-pointer hover:bg-gray-600 "
+                  }
             >{value}</Link>
-        </div>
+    </>
 }
 
 export default ReusableButton;
