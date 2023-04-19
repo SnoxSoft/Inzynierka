d6d7f46a964ddae1d49df107b59a8bf02e5e8b7c
Fancia666 <s18425@pjwstk.edu.pl>
2023-04-19 18:48:57 +0200

    Nagłówek - Header

--- Pirsoft.Frontend/src/components/base/Header.js
+++ Pirsoft.Frontend/src/components/base/Header.js
@@ -20,26 +20,31 @@ const Header = () => {
     return <>
         <div className={"grid grid-cols-1 grid-rows-1 place-items-end items-center min-h-min max-h-min p-2"}>
             <div className={"col-start-1 row-start-1 place-self-center"}>
-                <ReusableButton value={appName} link={"/"} />
+                <ReusableButton id={"header-home"} value={appName} link={"/"} />
             </div>
 
                 {sessionStorage.getItem('USER') ?
                     <div className={"col-start-1 col-end-1 row-start-1 row-end-1 flex flex-row"}>
                         
                         <ReusableButton
-                                 onClick={() => setIsOpen(true)}
-                                 value={<HiOutlineExclamationCircle size={40}/>}
-                                 link={'/notifications'}/>
+                            id={"header-notifications"}
+                            onClick={() => setIsOpen(true)}
+                            value={<HiOutlineExclamationCircle size={40}/>}
+                            link={'/notifications'}/>
 
-
-                        <ReusableButton value={
+                        <ReusableButton
+                            id={"header-profile"}
+                            value={
                             sessionStorage.getItem('AVATAR') !== null ?
                                     <img src={"data:image/png;base64," + sessionStorage.getItem('AVATAR')}
                                          alt="Avatar img" className={"w-10 rounded-2xl"}/>
                                     : avatarAlterText
                             } link={'/employee/'+sessionStorage.getItem('USER')} onClick={() => window.location.reload()}/>
-                        <ReusableButton value={<FiLogOut  size={30}/>}
-                                        onClick={() => {
+
+                        <ReusableButton
+                            id={"header-log-out"}
+                            value={<FiLogOut  size={30}/>}
+                            onClick={() => {
                                             sessionStorage.removeItem('USER');
                                             navigate("/", { replace: true });
                                             window.location.reload(false);
