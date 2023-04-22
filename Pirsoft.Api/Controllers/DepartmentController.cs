bc1fc50b01d5ac968608f561b446be4a2545342a
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-04-22 12:46:40 +0200

    TEST-49 created test & endpoint for getting a lit of all departments

---
+++ Pirsoft.Api/Controllers/DepartmentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pirsoft.Api.DatabaseManagement;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.Controllers;
+
+[ApiController]
+public class DepartmentController : ControllerBase
+{
+    private readonly ICrudHandler _crudHandler;
+    
+    public DepartmentController(ICrudHandler crudHandler) => _crudHandler = crudHandler;
+
+    [HttpGet("/get/departments")]
+    public IEnumerable<DepartmentModel> GetListOfAllDepartments() => 
+        _crudHandler.ReadAll<DepartmentModel>().OrderBy(p => p.department_id);
+    
+}
---
+++ Pirsoft.IntegrationTests/DepartmentControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Pirsoft.IntegrationTests;
+
+public class DepartmentControllerTests
+{
+    private readonly HttpClient _client = new();
+    
+    [Test]
+    public void GetAllDepartmentsIsReturningSuccessCodeWithResponse()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/departments");
+
+        response.Result.Content.Should().NotBeNull();
+        response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+}
