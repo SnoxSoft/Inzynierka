474776edd3484fe8cce00f4aac4404492267c306
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-04-22 13:10:28 +0200

    TEST-135 created test & endpoint for getting a list of all contract types

---
+++ Pirsoft.Api/Controllers/ContractTypeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pirsoft.Api.DatabaseManagement;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.Controllers;
+
+[ApiController]
+public class ContractTypeController
+{
+    private readonly ICrudHandler _crudHandler;
+    
+    public ContractTypeController(ICrudHandler crudHandler) => _crudHandler = crudHandler;
+    
+    [HttpGet("/get/contracts")]
+    public IEnumerable<ContractTypeModel> GetListOfAllContracts() =>
+        _crudHandler.ReadAll<ContractTypeModel>().OrderBy(contractType => contractType.contract_id);
+}
---
+++ Pirsoft.IntegrationTests/ContractTypeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Pirsoft.IntegrationTests;
+
+public class ContractTypeControllerTests
+{
+    private readonly HttpClient _client = new();
+    
+    [Test]
+    public void GetAllContractTypesIsReturningSuccessCodeWithResponse()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/contracts");
+
+        response.Result.Content.Should().NotBeNull();
+        response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+}
