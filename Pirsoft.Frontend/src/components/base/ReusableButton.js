a19b626d08c682bc847db53fb69246e45c440bb8
Fancia666 <s18425@pjwstk.edu.pl>
2023-04-23 15:07:22 +0200

    Zmiana guzika ReusableButton
    ReusableButton od teraz korzysta z hooka navigate

--- Pirsoft.Frontend/src/components/base/ReusableButton.js
+++ Pirsoft.Frontend/src/components/base/ReusableButton.js
@@ -1,20 +1,25 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
-const ReusableButton = ({value, link, onClick, color = "bg-brown-menu", formatting = "border-2 border-b-workday min-w-min w-24 h-12", id = ""}) => {
 
-    // let buttonFormat = typeof value === 'string'?
-    //     "grid grid-cols-1 place-content-center justify-self-center " :
-    //     "grid grid-rows-1 place-content-center place-items-center "
+const ReusableButton = ({value, link = undefined, onClick,
+        color = "bg-brown-menu", id = "",
+        formatting = "border-2 border-b-workday min-w-min w-24 h-12"}) => {
 
-    return <>
-            <Link id={id}
-                  onClick={onClick}
-                  to={link}
-                  className={
-                      color + " " + formatting + " rounded-md grid grid-rows-1 place-items-center text-workday font-bold text-center hover:cursor-pointer hover:bg-gray-600 "
-                  }
-            >{value}</Link>
-    </>
+    const navigate = useNavigate();
+
+    return <div
+        id={id}
+        onClick={() => {
+            if(link){
+                navigate(link);
+            }
+            else{
+                onClick() }
+        }}
+        className={
+            color + " " + formatting + " rounded-md grid grid-rows-1 place-items-center text-workday font-bold text-center hover:cursor-pointer hover:bg-gray-600 "
+        }
+    >{value}</div>
 }
 
 export default ReusableButton;
