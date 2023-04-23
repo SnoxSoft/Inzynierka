34a7e0ee59fe90c3246ce6fb7e8cd6db8c1cc62d
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-04-23 08:27:54 +0200

    TEST-136 created test & endpoint for getting a list of all company roles

---
+++ Pirsoft.Api/Controllers/CompanyRoleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pirsoft.Api.DatabaseManagement;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.Controllers;
+
+[ApiController]
+public class CompanyRoleController
+{
+    private readonly ICrudHandler _crudHandler;
+    
+    public CompanyRoleController(ICrudHandler crudHandler) => _crudHandler = crudHandler;
+
+    [HttpGet("/get/company/roles")]
+    public IEnumerable<CompanyRoleModel> GetListOfAllCompanyRoles() =>
+        _crudHandler.ReadAll<CompanyRoleModel>().OrderBy(companyRoleModel => companyRoleModel.role_id);
+}
---
+++ Pirsoft.IntegrationTests/CompanyRoleControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Pirsoft.IntegrationTests;
+
+public class CompanyRoleControllerTests
+{
+    private readonly HttpClient _client = new();
+    
+    [Test]
+    public void GetAllCompanyRoles_IsReturning_SuccessCodeWithResponse()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/company/roles");
+
+        response.Result.Content.Should().NotBeNull();
+        response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+}
