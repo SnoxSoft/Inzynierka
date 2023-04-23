c7ae68f64984d9fcbd6f0a4c9fa16eaa326bce35
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-04-23 18:53:23 +0200

    TEST-139 PoC of email sending endpoint service

---
+++ Pirsoft.Api/Controllers/PasswordManagementController.cs
@@ -0,0 +1,21 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Pirsoft.Api.Services;
+
+namespace Pirsoft.Api.Controllers;
+
+[ApiController]
+public class PasswordManagementController : ControllerBase
+{
+    private readonly EmailService _emailService;
+
+    public PasswordManagementController(EmailService emailService) => _emailService = emailService;
+    
+    [HttpPost("/reset/password/email")]
+    public async Task SendEmailWithPasswordResetLink()
+    {
+        await _emailService.SendEmailAsync("test@gmail.com", "subject", "message");
+    }
+}
