735ed2b810778b60b8db25f25867f89a32ab53b6
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-04-23 09:22:36 +0200

    TEST-138 added test & endpoint for getting a list of all seniority levels

---
+++ Pirsoft.Api/Controllers/SeniorityLevelController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pirsoft.Api.DatabaseManagement;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.Controllers;
+
+[ApiController]
+public class SeniorityLevelController
+{
+    private readonly ICrudHandler _crudHandler;
+    
+    public SeniorityLevelController(ICrudHandler crudHandler) => _crudHandler = crudHandler;
+
+    [HttpGet("/get/seniority/levels")]
+    public IEnumerable<SeniorityLevelModel> GetListOfAllSeniorityLevels() =>
+        _crudHandler.ReadAll<SeniorityLevelModel>()
+            .OrderBy(seniorityLevelModel => seniorityLevelModel.seniority_level_id);
+}
---
+++ Pirsoft.IntegrationTests/SeniorityLevelControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Pirsoft.IntegrationTests;
+
+public class SeniorityLevelControllerTests
+{
+    private readonly HttpClient _client = new();
+    
+    [Test]
+    public void GetAllSeniorityLevels_IsReturning_SuccessCodeWithResponse()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/seniority/levels");
+
+        response.Result.Content.Should().NotBeNull();
+        response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+}
