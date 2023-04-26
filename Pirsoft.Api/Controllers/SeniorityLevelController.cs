d301d39fc1de2e065791792996d1df37f362135a
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-04-26 14:20:32 +0200

    TEST-142 refactored all ReadAll usages to ReadAllAsync & fixed circular referce in seniorty level endpoint

--- Pirsoft.Api/Controllers/SeniorityLevelController.cs
+++ Pirsoft.Api/Controllers/SeniorityLevelController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 using Pirsoft.Api.DatabaseManagement;
 using Pirsoft.Api.Models;
 
@@ -12,7 +14,14 @@ public class SeniorityLevelController : Controller
     public SeniorityLevelController(ICrudHandler crudHandler) => _crudHandler = crudHandler;
 
     [HttpGet("/get/seniority/levels")]
-    public IEnumerable<SeniorityLevelModel> GetListOfAllSeniorityLevels() =>
-        _crudHandler.ReadAll<SeniorityLevelModel>()
-            .OrderBy(seniorityLevelModel => seniorityLevelModel.seniority_level_id);
+    public async Task<IEnumerable<SeniorityLevelModel>> GetListOfAllSeniorityLevels()
+    {
+        var query = await _crudHandler.ReadAllAsync<SeniorityLevelModel>();
+        var options = new JsonSerializerOptions
+        {
+            ReferenceHandler = ReferenceHandler.Preserve
+        };
+        var json = JsonSerializer.Serialize(query.OrderBy(seniorityLevelModel => seniorityLevelModel.seniority_level_id), options);
+        return JsonSerializer.Deserialize<IEnumerable<SeniorityLevelModel>>(json, options)!;
+    }
 }
