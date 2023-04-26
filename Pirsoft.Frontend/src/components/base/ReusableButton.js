e1b36edd0dcebc0086712d327645406f096e77d9
Fancia666 <s18425@pjwstk.edu.pl>
2023-04-26 22:38:13 +0200

    Poprawki dla Michała

--- Pirsoft.Frontend/src/components/base/ReusableButton.js
+++ Pirsoft.Frontend/src/components/base/ReusableButton.js
@@ -1,25 +1,15 @@
 import {Link, useNavigate} from "react-router-dom";
 
-
 const ReusableButton = ({value, link = undefined, onClick,
         color = "bg-brown-menu", id = "",
         formatting = "border-2 border-b-workday min-w-min w-24 h-12"}) => {
 
     const navigate = useNavigate();
 
-    return <div
-        id={id}
-        onClick={() => {
-            if(link){
-                navigate(link);
-            }
-            else{
-                onClick() }
-        }}
-        className={
-            color + " " + formatting + " rounded-md grid grid-rows-1 place-items-center text-workday font-bold text-center hover:cursor-pointer hover:bg-gray-600 "
-        }
-    >{value}</div>
+    return <div id={id} onClick={() => {if(link){navigate(link);} else{onClick();}}}
+                className={color + " " + formatting + " rounded-md grid grid-rows-1 place-items-center " +
+                    "text-workday font-bold text-center hover:cursor-pointer hover:bg-gray-600 "}>
+            {value}
+        </div>
 }
-
 export default ReusableButton;
--- Pirsoft.Frontend/src/components/employee/fields/Pesel.js
+++ Pirsoft.Frontend/src/components/employee/fields/Pesel.js
@@ -2,7 +2,6 @@ import TextInputEmployee from "./models/TextInputEmployee";
 
 const Pesel = ({id, onChange, value, disableChange}) => {
 
-
     return <TextInputEmployee id={id} onChange={onChange} value={value} disableChange={disableChange}
               type={"number"} maxLength={11}/>
 }
