404e131c5787e874ecb9987a66d097bf1c1e1172
Rominski, Kamil <kamil.rominski@gmail.com>
2023-05-01 23:26:28 +0200

    Added integration tests for GetDepartmentById

--- Pirsoft.IntegrationTests/DepartmentControllerTests.cs
+++ Pirsoft.IntegrationTests/DepartmentControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -14,6 +15,35 @@ public class DepartmentControllerTests
         var response = _client.GetAsync("https://localhost:7120/get/departments");
 
         response.Result.Content.Should().NotBeNull();
+        response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+
+    [Test]
+    public void GetDepartmentById_IsSuccessful_ForExistingDepartment()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/department/1");
+
+        response.Result.Content.Should().NotBeNull();
+        response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+
+    [Test]
+    public void GetDepartmentById_IsUnsuccessful_ForNonNumberId()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/company/abc");
+
+        response.Result.IsSuccessStatusCode.Should().BeFalse();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+
+    [Test]
+    public void GetDepartmentById_IsReturningNotFound()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/company/58462835682");
+
+        response.Result.Should().HaveStatusCode(HttpStatusCode.NoContent);
         response.Result.IsSuccessStatusCode.Should().BeTrue();
         response.IsCompletedSuccessfully.Should().BeTrue();
     }
