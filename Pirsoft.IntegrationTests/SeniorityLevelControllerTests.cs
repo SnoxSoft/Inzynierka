15a21d63dc15d4126f31b32b6d74ba9eb585fc2a
Rominski, Kamil <kamil.rominski@gmail.com>
2023-05-02 23:20:55 +0200

    Added integration tests for GetSeniorityLevelById

--- Pirsoft.IntegrationTests/SeniorityLevelControllerTests.cs
+++ Pirsoft.IntegrationTests/SeniorityLevelControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -14,6 +15,35 @@ public class SeniorityLevelControllerTests
         var response = _client.GetAsync("https://localhost:7120/get/seniority/levels");
 
         response.Result.Content.Should().NotBeNull();
+        response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+
+    [Test]
+    public void GetSeniorityLevelById_IsSuccessful_ForExistingContractType()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/seniority/level/1");
+
+        response.Result.Content.Should().NotBeNull();
+        response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+
+    [Test]
+    public void GetSeniorityLevelById_IsUnSuccesful_ForNonNumberId()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/seniority/level/abc");
+
+        response.Result.IsSuccessStatusCode.Should().BeFalse();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+
+    [Test]
+    public void GetSeniorityLevelById_IsReturningNotFound()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/seniority/level/999999");
+
+        response.Result.Should().HaveStatusCode(HttpStatusCode.NoContent);
         response.Result.IsSuccessStatusCode.Should().BeTrue();
         response.IsCompletedSuccessfully.Should().BeTrue();
     }
