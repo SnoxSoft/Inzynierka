a4f2b860e9c573691c1f4ee2dbfdbdbe4aca56b1
Rominski, Kamil <kamil.rominski@gmail.com>
2023-05-02 22:55:06 +0200

    Added integration tests for GetContractById

--- Pirsoft.IntegrationTests/ContractTypeControllerTests.cs
+++ Pirsoft.IntegrationTests/ContractTypeControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -15,6 +16,35 @@ public class ContractTypeControllerTests
 
         response.Result.Content.Should().NotBeNull();
         response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+
+    [Test]
+    public void GetContractById_IsSuccessful_ForExistingContractType()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/contract/1");
+
+        response.Result.Content.Should().NotBeNull();
+        response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+
+    [Test]
+    public void GetContractById_IsUnSuccesful_ForNonNumberId()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/contract/abc");
+
+        response.Result.IsSuccessStatusCode.Should().BeFalse();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+
+    [Test]
+    public void GetContractById_IsReturningNotFound()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/contract/999999");
+
+        response.Result.Should().HaveStatusCode(HttpStatusCode.NotFound);
+        response.Result.IsSuccessStatusCode.Should().BeFalse();
         response.IsCompletedSuccessfully.Should().BeTrue();
     }
 }
