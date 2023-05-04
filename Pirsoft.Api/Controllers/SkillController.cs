9a9b16e7815180def14805c54a40ee22668a5317
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-05-04 14:54:35 +0200

    TEST-162 added endpoint for displaying list of all skills & tests

---
+++ Pirsoft.Api/Controllers/SkillController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pirsoft.Api.DatabaseManagement;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.Controllers;
+
+[ApiController]
+public class SkillController : ControllerBase
+{
+    private readonly ICrudHandler _crudHandler;
+    
+    public SkillController(ICrudHandler crudHandler) => _crudHandler = crudHandler;
+    
+    [HttpGet("/get/skills")]
+    public async Task<IEnumerable<SkillModel>> GetListOfAllSkills()
+    {
+        var query = await _crudHandler.ReadAllAsync<SkillModel>();
+        return await query.OrderBy(skillModel => skillModel.skill_id).ToListAsync();
+    }
+}
