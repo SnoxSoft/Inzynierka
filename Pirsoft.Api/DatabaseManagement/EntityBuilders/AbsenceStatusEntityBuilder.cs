fbefcde92c46c81dcc75768b6b8a16712bf0b556
giedrys-michal <giedrys.michal@gmail.com>
2023-05-05 18:05:24 +0200

    Pirsoft.Api: Utworzenie testów jednostkowych klasy AbsenceStatusEntityBuilder
    Utworzenie testów jednostkowych dla klasy AbsenceStatusEntityBuilder wraz z jej pustą implementacją.

---
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/AbsenceStatusEntityBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
+{
+    public class AbsenceStatusEntityBuilder : EntityBuilderBase<AbsenceStatusModel>
+    {
+        public AbsenceStatusEntityBuilder(ModelBuilder modelBuilder) : base(modelBuilder) { }
+
+        public override ModelBuilder Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
