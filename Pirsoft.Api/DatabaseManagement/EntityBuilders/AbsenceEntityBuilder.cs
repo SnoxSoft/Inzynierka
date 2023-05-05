8f8e2c97496f853695a1176b1f806c91e3ca5c8f
giedrys-michal <giedrys.michal@gmail.com>
2023-05-05 18:21:34 +0200

    Pirsoft.Api: Aktualizacja istniejących klas z przestrzeni EntityBuilders

--- Pirsoft.Api/DatabaseManagement/EntityBuilders/AbsenceEntityBuilder.cs
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/AbsenceEntityBuilder.cs
@@ -13,17 +13,33 @@ namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
                 entity.HasKey(e => e.absence_id).HasName("PRIMARY");
 
                 entity.HasIndex(e => e.absence_id, "Id_UNIQUE").IsUnique();
-
-                entity.HasIndex(e => e.absence_employee_id, "fk_absence_employee_idx");
+                entity.HasIndex(e => e.absence_type_id, "fk_absence_absence_type_idx");
+                entity.HasIndex(e => e.employee_approver_id, "fk_absence_approver_idx");
+                entity.HasIndex(e => e.employee_owner_id, "fk_absence_employee_idx");
+                entity.HasIndex(e => e.absence_status_id, "fk_absence_status_idx");
 
                 entity.Property(e => e.absence_end_date).HasColumnType("date");
                 entity.Property(e => e.absence_start_date).HasColumnType("date");
 
-                entity.HasOne(d => d.absence_employee).WithMany(p => p.absences)
-                    .HasPrincipalKey(p => p.employee_id)
-                    .HasForeignKey(d => d.absence_employee_id)
+                entity.HasOne(d => d.absence_status).WithMany(p => p.absences)
+                    .HasForeignKey(d => d.absence_status_id)
                     .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("fk_absence_employee");
+                    .HasConstraintName("fk_absence_absence_status");
+
+                entity.HasOne(d => d.absence_type).WithMany(p => p.absences)
+                    .HasForeignKey(d => d.absence_type_id)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_absence_absence_type");
+
+                entity.HasOne(d => d.employee_approver).WithMany(p => p.absenceemployee_approvers)
+                    .HasForeignKey(d => d.employee_approver_id)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_absence_employee_approver");
+
+                entity.HasOne(d => d.employee_owner).WithMany(p => p.absenceemployee_owners)
+                    .HasForeignKey(d => d.employee_owner_id)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_absence_employee_owner");
             });
     }
 }
