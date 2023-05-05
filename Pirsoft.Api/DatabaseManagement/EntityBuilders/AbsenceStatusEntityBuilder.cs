5f094f3ba4300a598eb139bfbe42ed49ee1be994
giedrys-michal <giedrys.michal@gmail.com>
2023-05-05 18:07:43 +0200

    Pirsoft.Api: Implementacja klasy AbsenceStatusEntityBuilder zgodnie z testami

--- Pirsoft.Api/DatabaseManagement/EntityBuilders/AbsenceStatusEntityBuilder.cs
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/AbsenceStatusEntityBuilder.cs
@@ -8,8 +8,16 @@ namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
         public AbsenceStatusEntityBuilder(ModelBuilder modelBuilder) : base(modelBuilder) { }
 
         public override ModelBuilder Build()
-        {
-            throw new NotImplementedException();
-        }
+            => _modelBuilder.Entity<AbsenceStatusModel>(entity =>
+            {
+                entity.HasKey(e => e.absence_status_id).HasName("PRIMARY");
+
+                entity.HasIndex(e => e.absence_status_id, "absence_status_id_UNIQUE").IsUnique();
+                entity.HasIndex(e => e.absence_status_name, "absence_status_name_UNIQUE").IsUnique();
+                entity.HasIndex(e => e.absence_status_name_eng, "absence_status_name_eng_UNIQUE").IsUnique();
+
+                entity.Property(e => e.absence_status_name).HasMaxLength(45);
+                entity.Property(e => e.absence_status_name_eng).HasMaxLength(45);
+            });
     }
 }
