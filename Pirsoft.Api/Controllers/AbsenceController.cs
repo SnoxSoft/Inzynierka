75f751c1a39308bf5d20677b77e9bb51f60a2d70
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-05-06 12:41:03 +0200

    TEST-163 added get list of all absence statuses endpoint + test

---
+++ Pirsoft.Api/Controllers/AbsenceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pirsoft.Api.DatabaseManagement;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.Controllers;
+
+[ApiController]
+public class AbsenceController : ControllerBase
+{
+    private readonly ICrudHandler _crudHandler;
+    
+    public AbsenceController(ICrudHandler crudHandler) => _crudHandler = crudHandler;
+    
+    [HttpGet("get/absence/statuses")]
+    public async Task<IEnumerable<AbsenceStatusModel>> GetListOfAllAbsenceStatuses()
+    {
+        var query = await _crudHandler.ReadAllAsync<AbsenceStatusModel>();
+        return await query.OrderBy(absenceStatus => absenceStatus.absence_status_id).ToListAsync();
+    }
+}
---
+++ Pirsoft.IntegrationTests/AbsenceControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Pirsoft.IntegrationTests;
+
+public class AbsenceControllerTests
+{
+    private readonly HttpClient _client = new();
+    
+    [Test]
+    public void GetAllAbsenceStatuses_IsReturning_SuccessCodeWithResponse()
+    {
+        var response = _client.GetAsync("https://localhost:7120/get/absence/statuses");
+
+        response.Result.Content.Should().NotBeNull();
+        response.Result.IsSuccessStatusCode.Should().BeTrue();
+        response.IsCompletedSuccessfully.Should().BeTrue();
+    }
+}
