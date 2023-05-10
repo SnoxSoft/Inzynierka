11b23b1512c37901a272a72814dd9ca6a1bf95fa
Fancia666 <s18425@pjwstk.edu.pl>
2023-05-10 17:25:40 +0200

    Added integration tests for Creating Absence

--- Pirsoft.IntegrationTests/AbsenceControllerTests.cs
+++ Pirsoft.IntegrationTests/AbsenceControllerTests.cs
@@ -1,6 +1,12 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
 using FluentAssertions;
+using Newtonsoft.Json;
 using NUnit.Framework;
+using Pirsoft.Api.Models;
 
 namespace Pirsoft.IntegrationTests;
 
@@ -27,4 +33,27 @@ public class AbsenceControllerTests
         response.Result.IsSuccessStatusCode.Should().BeTrue();
         response.IsCompletedSuccessfully.Should().BeTrue();
     }
+    
+    [Test]
+    public async Task CreateNewAbsence_IsReturning_SuccessCodeWithValidParameters()
+    {
+        AbsenceModel absenceModel = new AbsenceModel()
+        {
+            absence_start_date = new DateTime(2023,05,11),
+            absence_end_date = new DateTime(2023,05,12),
+            unpaid = Convert.ToSByte(1),
+            duration = 0,
+            absence_id = 1,
+            absence_type_id = 1,
+            employee_approver_id = 1,
+            employee_owner_id = 1,
+            absence_status_id = 1,
+        };
+    
+        var response = await _client.PostAsJsonAsync("https://localhost:7120/create/new/absence", absenceModel);
+    
+        response.IsSuccessStatusCode.Should().BeTrue();
+        response.EnsureSuccessStatusCode();
+    }
+    
 }
