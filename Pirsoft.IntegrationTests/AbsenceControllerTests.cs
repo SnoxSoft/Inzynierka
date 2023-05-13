095eaf05fffdb21e737ba1cff0104bdd66dc8881
Fancia666 <s18425@pjwstk.edu.pl>
2023-05-13 18:18:33 +0200

    Making tests for editing and deleting absences

--- Pirsoft.IntegrationTests/AbsenceControllerTests.cs
+++ Pirsoft.IntegrationTests/AbsenceControllerTests.cs
@@ -1,12 +1,10 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
-using System.Net.Http.Json;
-using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
 using NUnit.Framework;
-using Pirsoft.Api.Models;
 
 namespace Pirsoft.IntegrationTests;
 
@@ -35,9 +33,26 @@ public class AbsenceControllerTests
     }
     
     [Test]
+    public void DeleteAbsence_IsReturning_NotASucces()
+    {
+        var response = _client.DeleteAsync("https://localhost:7120/delete/absence/6");
+        
+        response.IsCompletedSuccessfully.Should().BeFalse();
+    }
+    
+    [Test]
+    public async Task EditAbsence_IsReturning_Succes()
+    {
+        var response = await _client.PutAsync("https://localhost:7120/edit/absence/2?employeeApproverId=2&absenceStatusId=3", null);
+        
+        Console.WriteLine(response.StatusCode);
+        Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
+    }
+
+    [Test]
     public async Task CreateNewAbsence_IsReturning_SuccessCodeWithValidParameters()
     {
-        var response = await _client.PostAsync("https://localhost:7120/create/new/absence?absenceStartDate=2023-05-05&absenceEndDate=2023-05-05&unpaid=1&absenceTypeId=1&employeeApproverId=1&employeeOwnerId=1&absenceStatusId=1", null);
+        var response = await _client.PostAsync("https://localhost:7120/create/new/absence?absenceStartDate=2023-05-05&absenceEndDate=2023-05-05&unpaid=1&absenceTypeId=1&employeeApproverId=1&employeeOwnerId=2&absenceStatusId=1", null);
     
         response.IsSuccessStatusCode.Should().BeTrue();
         response.EnsureSuccessStatusCode();
