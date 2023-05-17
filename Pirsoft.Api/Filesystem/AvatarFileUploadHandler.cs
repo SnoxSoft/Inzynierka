699c1d82818ce248b98e49f33c6550962fffa313
giedrys-michal <giedrys.michal@gmail.com>
2023-05-17 19:53:12 +0200

    Pirsoft.Api/UT: Przygotowanie testów dla klasy AvatarFileUploadHandler
    Utworzenie testów jednostkowych dla klasy AvatarFileUploadHandler oraz przygotowanie pustej implementacji klasy.

---
+++ Pirsoft.Api/Filesystem/AvatarFileUploadHandler.cs
@@ -0,0 +1,28 @@
+﻿using Pirsoft.Api.Validators;
+
+namespace Pirsoft.Api.Filesystem
+{
+    public class AvatarFileUploadHandler : IAvatarFileUploadHandler
+    {
+        private readonly IAvatarFileValidator _avatarFileVlidator = null!;
+        private readonly IConfiguration _globalAppSettings = null!;
+
+        public AvatarFileUploadHandler(
+            IAvatarFileValidator avatarFileVlidator,
+            IConfiguration globalAppSettings)
+        {
+            _avatarFileVlidator = avatarFileVlidator;
+            _globalAppSettings = globalAppSettings;
+        }
+
+        public async Task<string> Upload(IFormCollection formData)
+        {
+            return await Task.FromException<string>(new NotImplementedException());
+        }
+    }
+
+    public interface IAvatarFileUploadHandler
+    {
+        Task<string> Upload(IFormCollection formData);
+    }
+}
