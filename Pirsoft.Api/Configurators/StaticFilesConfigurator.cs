e08d9043bc4c48a38919676143f0b5f49d908f95
giedrys-michal <giedrys.michal@gmail.com>
2023-05-17 23:09:04 +0200

    Pirsoft.Api: Implementacja klasy StaticFilesConfigurator zgodnie z testami

--- Pirsoft.Api/Configurators/StaticFilesConfigurator.cs
+++ Pirsoft.Api/Configurators/StaticFilesConfigurator.cs
@@ -1,12 +1,36 @@
-﻿using Pirsoft.Api.PatternsAbstraction;
+﻿using Microsoft.Extensions.FileProviders;
+using Pirsoft.Api.PatternsAbstraction;
 
 namespace Pirsoft.Api.Configurators
 {
     public class StaticFilesConfigurator : SingletonBase<StaticFilesConfigurator>
     {
+        private const string AVATAR_DIRECTORY = "Resources:AvatarDirectory";
+
         public void Init(IApplicationBuilder appBuilder, IConfiguration config)
         {
-            throw new NotImplementedException();
+            string avatarSubdirectory = config.GetValue<string>(AVATAR_DIRECTORY);
+
+            string fullPathToAvatarDirectory = ensureDirectoryExists(avatarSubdirectory);
+
+            appBuilder.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(fullPathToAvatarDirectory),
+                RequestPath = $"/{avatarSubdirectory}",
+            });
+        }
+
+        private string ensureDirectoryExists(string subdirectory)
+        {
+            string projectRootDirectory = Directory.GetParent(Directory.GetCurrentDirectory())!.FullName;
+            string fullDirectoryPath = Path.Combine(projectRootDirectory, subdirectory);
+
+            if (Directory.Exists(fullDirectoryPath) == false)
+            {
+                Directory.CreateDirectory(fullDirectoryPath);
+            }
+
+            return fullDirectoryPath;
         }
     }
 }
