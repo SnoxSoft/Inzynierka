0d34ca99c73f443a7f489b2d04e2a702920025f4
giedrys-michal <giedrys.michal@gmail.com>
2023-05-17 23:04:53 +0200

    Pirsoft.Api/UT: Przygotowanie testów klasy StaticFilesConfigurator
    Przygotowanie klasy testów jednostkowych wraz z pustą implementacją klasy StaticFilesConfigurator.

---
+++ Pirsoft.Api/Configurators/StaticFilesConfigurator.cs
@@ -0,0 +1,12 @@
+﻿using Pirsoft.Api.PatternsAbstraction;
+
+namespace Pirsoft.Api.Configurators
+{
+    public class StaticFilesConfigurator : SingletonBase<StaticFilesConfigurator>
+    {
+        public void Init(IApplicationBuilder appBuilder, IConfiguration config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pirsoft.UnitTests/Configurators/StaticFilesConfiguratorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+using Pirsoft.Api.Configurators;
+
+namespace Pirsoft.UnitTests.Configurators
+{
+    [TestFixture]
+    public class StaticFilesConfiguratorTests : BaseForConfiguratorTests<StaticFilesConfigurator>
+    {
+        [Test]
+        public void Init_ShouldConfigureStaticFiles()
+        {
+            //Arrange
+            Mock<IApplicationBuilder> appBuilderMock = new();
+
+            var fakeInMemorySettings = new Dictionary<string, string>()
+            {
+                { "Resources:AvatarDirectory", "fake\\directory" }
+            };
+
+            IConfiguration fakeGlobalAppSettings = new ConfigurationBuilder()
+                .AddInMemoryCollection(fakeInMemorySettings)
+                .Build();
+
+            //Act
+            sut.Init(appBuilderMock.Object, fakeGlobalAppSettings);
+
+            //Assert
+            appBuilderMock
+                .Verify(m => m.Use(It.IsAny<Func<RequestDelegate, RequestDelegate>>()), Times.Once);
+        }
+    }
+}
