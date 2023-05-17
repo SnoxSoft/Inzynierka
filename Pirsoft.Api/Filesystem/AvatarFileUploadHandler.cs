7ace85dae00b8a108c4ec01f5590236b8da49c47
giedrys-michal <giedrys.michal@gmail.com>
2023-05-17 19:53:34 +0200

    Pirsoft.Api: Implementacja klasy AvatarFileUploadHandler zgodnie z testami.

--- Pirsoft.Api/Filesystem/AvatarFileUploadHandler.cs
+++ Pirsoft.Api/Filesystem/AvatarFileUploadHandler.cs
@@ -17,7 +17,55 @@ namespace Pirsoft.Api.Filesystem
 
         public async Task<string> Upload(IFormCollection formData)
         {
-            return await Task.FromException<string>(new NotImplementedException());
+            if (formData.Files == null || formData.Files.Any() == false)
+            {
+                throw new ArgumentException("Request body form data does not contain any files.", nameof(formData));
+            }
+
+            IFormFile postedAvatarFile = formData.Files[0];
+
+            if (_avatarFileVlidator.IsAvatarFileValid(postedAvatarFile) == false)
+            {
+                throw new ArgumentException("Passed file other than '.png' or '.jpg' or more than 2 MB in size.", nameof(formData));
+            }
+
+            string avatarSecureFilePath = prepareSecureFilePath(postedAvatarFile);
+
+            using (var stream = new FileStream(avatarSecureFilePath, FileMode.Create))
+            {
+                await postedAvatarFile.CopyToAsync(stream);
+            }
+
+            if (File.Exists(avatarSecureFilePath) == false)
+            {
+                throw new InvalidOperationException("Avatar file was not uploaded properly.");
+            }
+
+            return avatarSecureFilePath;
+        }
+
+        private string prepareSecureFilePath(IFormFile postedFile)
+        {
+            string fullPathToAvatarDirectory = ensureAvatarDirectoryExist();
+
+            string originalExtension = Path.GetExtension(postedFile.FileName);
+            string secureFileName = Path.ChangeExtension(Path.GetRandomFileName(), originalExtension);
+
+            return Path.Combine(fullPathToAvatarDirectory, secureFileName);
+        }
+
+        private string ensureAvatarDirectoryExist()
+        {
+            string projectRootDirectory = Directory.GetParent(Directory.GetCurrentDirectory())!.FullName;
+            string avatarDirectory = _globalAppSettings.GetValue<string>("Resources:AvatarDirectory");
+            string fullPathToAvatarDirectory = Path.Combine(projectRootDirectory, avatarDirectory);
+
+            if (Directory.Exists(fullPathToAvatarDirectory) == false)
+            {
+                Directory.CreateDirectory(fullPathToAvatarDirectory);
+            }
+
+            return fullPathToAvatarDirectory;
         }
     }
 
