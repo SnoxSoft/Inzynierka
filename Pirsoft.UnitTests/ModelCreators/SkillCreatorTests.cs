f6fc34178ff8df95943c70d882436a7e3371a9fd
giedrys-michal <giedrys.michal@gmail.com>
2023-05-20 22:40:34 +0200

    Pirsoft.Api/UT: Przygotowanie testów klasy SkillCreator
    Przygotowanie testów jednostkowych klasy SkillCreator wraz z jej pusta implementacją.
    Dodanie klasy enumeracji ESkill z nazwami i wartościami umiejętności.

---
+++ Pirsoft.UnitTests/ModelCreators/SkillCreatorTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Pirsoft.Api.Enums;
+using Pirsoft.Api.Models.ModelCreators;
+using Pirsoft.Api.Models;
+using System.Collections.Generic;
+using FluentAssertions;
+using System;
+
+namespace Pirsoft.UnitTests.ModelCreators
+{
+    public class SkillCreatorTests
+    {
+        [Test]
+        [TestCaseSource(nameof(GetTestData))]
+        public void CreateModel_CreatesCorrectModel(ESkill testData)
+        {
+            //Arrange
+            SkillCreator sut = new(testData);
+
+            //Act
+            IApiModel result = sut.CreateModel();
+
+            //Assert
+            result.Should().BeOfType<SkillModel>();
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            foreach (ESkill skill in Enum.GetValues(typeof(ESkill)))
+            {
+                yield return new object[] { skill };
+            }
+        }
+    }
+}
