10c28f14f6ffc988d532766bbd5dcb07e38f9672
giedrys-michal <giedrys.michal@gmail.com>
2023-05-20 21:29:01 +0200

    Pirsoft.UnitTests: Aktualizacja testów klasy EmployeeController
    Dodanie sztucznego kontekstu zapytania w celu zasymulowania otrzymania danych z formularza.

--- Pirsoft.UnitTests/ControllerMockTests/EmployeeControllerTests.cs
+++ Pirsoft.UnitTests/ControllerMockTests/EmployeeControllerTests.cs
@@ -96,7 +96,27 @@ public class EmployeeControllerTests
     {
         // Arrange
         var id = 1;
-        
+
+        Mock<IFormCollection> formCollectionMock = new();
+        formCollectionMock
+            .Setup(m => m.Files)
+            .Returns(new FormFileCollection());
+
+        Mock<HttpRequest> requestMock = new();
+        requestMock.Setup(m => m.Scheme).Returns("http");
+        requestMock.Setup(m => m.Host).Returns(HostString.FromUriComponent("http://localhost:7120"));
+        requestMock.Setup(m => m.PathBase).Returns(PathString.FromUriComponent("/"));
+        requestMock.Setup(m => m.Form).Returns(formCollectionMock.Object);
+
+        HttpContext fakeHttpContext = Mock.Of<HttpContext>(_ => _.Request == requestMock.Object);
+
+        ControllerContext fakeControllerContext = new()
+        {
+            HttpContext = fakeHttpContext,
+        };
+
+        _employeeController.ControllerContext = fakeControllerContext;
+
         var existingEmployee = new EmployeeModel { employee_id = id, first_name = "Jane", last_name = "Doe", pesel = "12345678901", email_address = "jane.doe@example.com" };
         _crudHandlerMock.Setup(m => m.ReadAsync<EmployeeModel>(id)).ReturnsAsync(existingEmployee);
 
