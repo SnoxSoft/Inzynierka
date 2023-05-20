9c2872dfc148611a88b824c3e54e84010afb835d
giedrys-michal <giedrys.michal@gmail.com>
2023-05-20 22:40:34 +0200

    Pirsoft.Api/UT: Przygotowanie testów klasy SkillCreator
    Przygotowanie testów jednostkowych klasy SkillCreator wraz z jej pusta implementacją.
    Dodanie klasy enumeracji ESkill z nazwami i wartościami umiejętności.

---
+++ Pirsoft.Api/Enums/ESkill.cs
@@ -0,0 +1,11 @@
+﻿namespace Pirsoft.Api.Enums
+{
+    public enum ESkill
+    {
+        HTML     = 1,
+        Java     = 2,
+        MsOffice = 3,
+        PHP      = 4,
+        SQL      = 5,
+    }
+}
