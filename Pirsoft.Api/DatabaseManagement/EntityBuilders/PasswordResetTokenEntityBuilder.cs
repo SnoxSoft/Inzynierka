5197496a3bb7ebbfec9fc1951be4b270304e63c3
giedrys-michal <giedrys.michal@gmail.com>
2023-05-20 20:54:25 +0200

    Pirsoft.Api/UT: Przygotowanie pominiętych testów dla klasy PasswordResetTokenEntityBuilder
    Przygotowanie klasy testów jednostkowych wraz z pustą implementacją klasy PasswordResetTokenEntityBuilder.

---
+++ Pirsoft.Api/DatabaseManagement/EntityBuilders/PasswordResetTokenEntityBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pirsoft.Api.Models;
+
+namespace Pirsoft.Api.DatabaseManagement.EntityBuilders
+{
+    public class PasswordResetTokenEntityBuilder : EntityBuilderBase<PasswordResetTokenModel>
+    {
+        public PasswordResetTokenEntityBuilder(ModelBuilder modelBuilder) : base(modelBuilder) { }
+
+        public override ModelBuilder Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pirsoft.UnitTests/DatabaseManagement/EntityBuilders/PasswordResetTokenEntityBuilderTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Pirsoft.Api.DatabaseManagement.EntityBuilders;
+
+namespace Pirsoft.UnitTests.DatabaseManagement.EntityBuilders
+{
+    public class PasswordResetTokenEntityBuilderTests
+    {
+        private ModelBuilder _fakeModelBuilder = null!;
+
+        [SetUp]
+        public void SetUp() => _fakeModelBuilder = new();
+
+        [Test]
+        public void Build_ReturnsUpdatedModelBuilder()
+        {
+            //Arrange
+            PasswordResetTokenEntityBuilder sut = new(_fakeModelBuilder);
+
+            //Act
+            ModelBuilder result = sut.Build();
+
+            //Assert
+            result.Should().BeEquivalentTo(_fakeModelBuilder);
+        }
+
+        [Test]
+        public void GetEntityPrimaryKey_ReturnsCorrectPrimaryKey()
+        {
+            //Arrange
+            string expectedPrimaryKeyName = "token_id";
+            PasswordResetTokenEntityBuilder sut = new(_fakeModelBuilder);
+
+            //Act
+            string result = sut.GetEntityPrimaryKey(sut.Build());
+
+            //Assert
+            result.Should().Be(expectedPrimaryKeyName);
+        }
+    }
+}
