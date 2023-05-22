ba05df4ba5b7b197587c6a9071b31d7369a5f09c
Rominski, Kamil <kamil.rominski@gmail.com>
2023-05-22 23:00:29 +0200

    each generated component matches regex, moved each generator to own files

---
+++ Pirsoft.ValidationTools/DatabaseEntryGenerator/bank_number_generator.py
@@ -0,0 +1,33 @@
+import numpy as np
+import random
+from schwifty import IBAN
+import re
+
+bank_regex = "^[0-9]+$"
+
+def generate(amount):
+    f = open("dane/bank_nr.txt", "a")
+    for x in range(amount):
+        while True:
+            weights = [3, 9, 7, 1, 3, 9, 7]
+            with open("dane/banki.txt", "r") as file:
+                lines = file.readlines()
+            bank_array = np.array([int(line) for line in lines])
+            rand_bank_array = random.randint(1, 29)
+            rand_domestic_code = random.randint(0, 999)
+            rand_domestic_code = str(rand_domestic_code).zfill(3)
+            bank_num = f'{bank_array[rand_bank_array]}{rand_domestic_code}'
+            bank_checksum = sum([int(bank_num[i]) * weights[i] for i in range(7)]) % 10
+            if bank_checksum != 0:
+                bank_checksum = 10 - bank_checksum
+            bank_num = f'{bank_num}{bank_checksum}'
+            rand_acc_num = random.randint(0, 9999999999999999)
+            rand_acc_num = str(rand_acc_num).zfill(16)
+            iban = IBAN.generate('PL', bank_num, str(rand_acc_num))
+            bank_number = str(iban)[2:]
+            if re.match(bank_regex, bank_number):
+                break
+        f.write(bank_number)
+        f.write("\n")
+    f.close()
+    print("{} numerow kont bankowych wygenerowanych ktore sa zgodne z regexem w API".format(str(amount)))
---
+++ Pirsoft.ValidationTools/DatabaseEntryGenerator/menu.py
@@ -0,0 +1,57 @@
+import os
+import password_generator
+import pesel_generator
+import bank_number_generator
+import employee_generator
+
+
+def clear_console():
+    os.system('cls')
+
+
+def main():
+    while True:
+        clear_console()
+        print("PIRSOFT - generator danych pracownikow")
+        print("Aby stworzyc pracownika musisz wpierw wygenerowac hasla, pesele oraz konta bankowe!\n")
+        print("Wybierz opcje: ")
+        print("1. Wygeneruj hasla")
+        print("2. Wygeneruj numery PESEL")
+        print("3. Wygeneruj numery kont bankowych")
+        print("4. Wygeneruj pracownika")
+        print("5. Wylacz program :C")
+        menu_input = input("\nPodaj numer wybranej opcji: ")
+        print(menu_input)
+        if menu_input == "1":
+            clear_console()
+            while True:
+                password_generated_amount = input("Podaj liczbe hasel do wygenerowania: ")
+                if password_generated_amount is not None:
+                    break
+            password_generator.generate(int(password_generated_amount))
+            input("Nacisnij dowolna litere aby kontynuowac...")
+        elif menu_input == "2":
+            clear_console()
+            while True:
+                pesel_generated_amount = input("Podaj liczbe numerow PESEL do wygenerowania: ")
+                if pesel_generated_amount is not None:
+                    break
+            pesel_generator.generate(int(pesel_generated_amount))
+            input("Nacisnij dowolna litere aby kontynuowac...")
+        elif menu_input == "3":
+            clear_console()
+            while True:
+                bank_generated_amount = input("Podaj liczbe numerow kont bankowych do wygenerowania: ")
+                if bank_generated_amount is not None:
+                    break
+            bank_number_generator.generate(int(bank_generated_amount))
+            input("Nacisnij dowolna litere aby kontynuowac...")
+        elif menu_input == "4":
+            employee_generator.generate()
+            input("Nacisnij dowolna litere aby kontynuowac...")
+        elif menu_input == "5":
+            break
+
+
+if __name__ == "__main__":
+    main()
