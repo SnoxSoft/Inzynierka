2e101f4b363b8841f156bacc1bb0d3ad2ca30469
giedrys-michal <giedrys.michal@gmail.com>
2023-05-25 22:10:50 +0200

    Pirsoft.Api/UT: Refaktor testów i klasy PasswordManagementController

--- Pirsoft.UnitTests/ControllerMockTests/PasswordManagementControllerTests.cs
+++ Pirsoft.UnitTests/ControllerMockTests/PasswordManagementControllerTests.cs
@@ -13,98 +13,95 @@ using Pirsoft.Api.Services;
 namespace Pirsoft.UnitTests.ControllerMockTests;
 
 [TestFixture]
-    public class PasswordManagementControllerTests
+public class PasswordManagementControllerTests
+{
+    private PasswordManagementController _controller;
+    private Mock<ICrudHandler> _crudHandlerMock;
+    private Mock<IEmployeeCrudHandler> _employeeCrudHandlerMock;
+    private Mock<IMailService> _emailServiceMock;
+    private Mock<IPasswordService> _passwordServiceMock;
+
+    [SetUp]
+    public void Setup()
     {
-        private PasswordManagementController _controller;
-        private Mock<ICrudHandler> _crudHandlerMock;
-        private Mock<IEmployeeCrudHandler> _employeeCrudHandlerMock;
-        private Mock<IUserManager<EmployeeModel>> _userManagerMock;
-        private Mock<IMailService> _emailServiceMock;
-        private Mock<IPasswordService> _passwordServiceMock;
+        _crudHandlerMock = new Mock<ICrudHandler>();
+        _employeeCrudHandlerMock = new Mock<IEmployeeCrudHandler>();
+        _emailServiceMock = new Mock<IMailService>();
+        _passwordServiceMock = new Mock<IPasswordService>();
 
-        [SetUp]
-        public void Setup()
-        {
-            _crudHandlerMock = new Mock<ICrudHandler>();
-            _employeeCrudHandlerMock = new Mock<IEmployeeCrudHandler>();
-            _userManagerMock = new Mock<IUserManager<EmployeeModel>>();
-            _emailServiceMock = new Mock<IMailService>();
-            _passwordServiceMock = new Mock<IPasswordService>();
-
-            _controller = new PasswordManagementController(
-                _crudHandlerMock.Object,
-                _userManagerMock.Object,
-                _emailServiceMock.Object,
-                _passwordServiceMock.Object,
-                _employeeCrudHandlerMock.Object
-            );
-        }
-
-        [Test]
-        public async Task ChangePasswordWithResetCode_ExpiredToken_ReturnsNotFound()
-        {
-            // Arrange
-            var resetToken = new PasswordResetTokenModel
-            {
-                expiration_time = DateTime.Now.AddHours(-25) // Expired token
-            };
-
-            _crudHandlerMock.Setup(mock => mock.ReadAsync<PasswordResetTokenModel>(It.IsAny<int>()))
-                .ReturnsAsync(resetToken);
-
-            // Act
-            var result = await _controller.ChangePasswordWithResetCode(123, "password", "password");
-
-            // Assert
-            result.Should().BeOfType<NotFoundResult>();
-        }
-
-        [Test]
-        public async Task ChangePasswordWithResetCode_PasswordsDoNotMatch_ReturnsBadRequest()
-        {
-            // Arrange
-            var resetToken = new PasswordResetTokenModel
-            {
-                expiration_time = DateTime.Now.AddHours(1) // Valid token
-            };
-
-            _crudHandlerMock.Setup(mock => mock.ReadAsync<PasswordResetTokenModel>(It.IsAny<int>()))
-                .ReturnsAsync(resetToken);
-
-            // Act
-            var result = await _controller.ChangePasswordWithResetCode(123, "password1", "password2");
-
-            // Assert
-            result.Should().BeOfType<BadRequestObjectResult>()
-                .Which.Value.Should().Be("New passwords do not match");
-        }
-
-        [Test]
-        public async Task ChangePasswordWithResetCode_ValidRequest_ReturnsOk()
-        {
-            // Arrange
-            var resetToken = new PasswordResetTokenModel
-            {
-                expiration_time = DateTime.Now.AddHours(1), // Valid token
-                token_employee_id = 456
-            };
-
-            var employee = new EmployeeModel
-            {
-                employee_id = 456
-            };
-
-            _crudHandlerMock.Setup(mock => mock.ReadAsync<PasswordResetTokenModel>(It.IsAny<int>()))
-                .ReturnsAsync(resetToken);
-            _crudHandlerMock.Setup(mock => mock.ReadAsync<EmployeeModel>(It.IsAny<int>()))
-                .ReturnsAsync(employee);
-            _crudHandlerMock.Setup(mock => mock.UpdateAsync(It.IsAny<EmployeeModel>()))
-                .Returns(Task.CompletedTask);
-
-            // Act
-            var result = await _controller.ChangePasswordWithResetCode(123, "password", "password");
-
-            // Assert
-            result.Should().BeOfType<OkResult>();
-        }
+        _controller = new PasswordManagementController(
+            _crudHandlerMock.Object,
+            _emailServiceMock.Object,
+            _passwordServiceMock.Object,
+            _employeeCrudHandlerMock.Object
+        );
     }
+
+    [Test]
+    public async Task ChangePasswordWithResetCode_ExpiredToken_ReturnsNotFound()
+    {
+        // Arrange
+        var resetToken = new PasswordResetTokenModel
+        {
+            expiration_time = DateTime.Now.AddHours(-25) // Expired token
+        };
+
+        _crudHandlerMock.Setup(mock => mock.ReadAsync<PasswordResetTokenModel>(It.IsAny<int>()))
+            .ReturnsAsync(resetToken);
+
+        // Act
+        var result = await _controller.ChangePasswordWithResetCode(123, "password", "password");
+
+        // Assert
+        result.Should().BeOfType<NotFoundResult>();
+    }
+
+    [Test]
+    public async Task ChangePasswordWithResetCode_PasswordsDoNotMatch_ReturnsBadRequest()
+    {
+        // Arrange
+        var resetToken = new PasswordResetTokenModel
+        {
+            expiration_time = DateTime.Now.AddHours(1) // Valid token
+        };
+
+        _crudHandlerMock.Setup(mock => mock.ReadAsync<PasswordResetTokenModel>(It.IsAny<int>()))
+            .ReturnsAsync(resetToken);
+
+        // Act
+        var result = await _controller.ChangePasswordWithResetCode(123, "password1", "password2");
+
+        // Assert
+        result.Should().BeOfType<BadRequestObjectResult>()
+            .Which.Value.Should().Be("New passwords do not match");
+    }
+
+    [Test]
+    public async Task ChangePasswordWithResetCode_ValidRequest_ReturnsOk()
+    {
+        // Arrange
+        var resetToken = new PasswordResetTokenModel
+        {
+            expiration_time = DateTime.Now.AddHours(1), // Valid token
+            token_employee_id = 456
+        };
+
+        var employee = new EmployeeModel
+        {
+            employee_id = 456
+        };
+
+        _crudHandlerMock.Setup(mock => mock.ReadAsync<PasswordResetTokenModel>(It.IsAny<int>()))
+            .ReturnsAsync(resetToken);
+        _crudHandlerMock.Setup(mock => mock.ReadAsync<EmployeeModel>(It.IsAny<int>()))
+            .ReturnsAsync(employee);
+        _crudHandlerMock.Setup(mock => mock.UpdateAsync(It.IsAny<EmployeeModel>()))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _controller.ChangePasswordWithResetCode(123, "password", "password");
+
+        // Assert
+        result.Should().BeOfType<OkResult>();
+    }
+}
