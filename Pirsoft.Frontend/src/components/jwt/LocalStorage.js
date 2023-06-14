8ed7bfad3f4babcad1aaf44df83c36535c25b59f
Rominski, Kamil <kamil.rominski@gmail.com>
2023-06-14 23:43:07 +0200

    zmiana czasu JWT

--- Pirsoft.Frontend/src/components/jwt/LocalStorage.js
+++ Pirsoft.Frontend/src/components/jwt/LocalStorage.js
@@ -1,4 +1,4 @@
-const TIME_TO_EXPIRY = 1000*60*15; //ms*s*m
+const TIME_TO_EXPIRY = 1000*60*60; //ms*s*m
 
 export function getLocalStorageKeyWithExpiry(key) {
     const localStr = localStorage.getItem(key)
